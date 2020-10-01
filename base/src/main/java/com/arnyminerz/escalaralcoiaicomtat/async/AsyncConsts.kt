f2e1ed547dc6eadf5c6a1c7ae03994ef7ee70e40
Arnau Mora <arnyminer.z@gmail.com>
2020-10-01 15:12:30 +0200

    :ambulance: Changed domain from `https://arnyminerz.com:3000` and `http://arnyminerz.com:3001` to simply `http` and `https` of `api.arnyminerz.com`.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/async/AsyncConsts.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/async/AsyncConsts.kt
@@ -1,4 +1,4 @@
 package com.arnyminerz.escalaralcoiaicomtat.async
 
-const val EXTENDED_API_URL = "https://arnyminerz.com:3001"
-const val EXTENDED_API_URL_NO_SECURE = "http://arnyminerz.com:3000"
+const val EXTENDED_API_URL = "https://api.arnyminerz.com"
+const val EXTENDED_API_URL_NO_SECURE = "http://api.arnyminerz.com"
