564b0bc5002f0411b428f186a96750dc5edbd7d2
Arnau Mora <arnyminer.z@gmail.com>
2021-02-26 23:09:55 +0100

    :sparkles: Added Github repo link in info page of settings.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/InfoSettingsFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/InfoSettingsFragment.kt
@@ -1,5 +1,7 @@
 package com.arnyminerz.escalaralcoiaicomtat.fragment.preferences
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
@@ -13,11 +15,19 @@ class InfoSettingsFragment : PreferenceFragmentCompat() {
 
         val versionPreference: Preference? = findPreference("pref_version")
         val buildPreference: Preference? = findPreference("pref_build")
+        val githubPreference: Preference? = findPreference("pref_github")
 
         val versionCode = BuildConfig.VERSION_CODE
         val versionName = BuildConfig.VERSION_NAME
 
         versionPreference?.summary = versionName
         buildPreference?.summary = versionCode.toString()
+
+        githubPreference?.setOnPreferenceClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse("https://github.com/Escalar-Alcoia-i-Comtat/Android")
+            })
+            true
+        }
     }
 }
