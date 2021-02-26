528383fe3f79447a034c88c437f74bddf600b862
Arnau Mora <arnyminer.z@gmail.com>
2021-02-26 13:11:00 +0100

    :sparkles: Implemented FeedbackActivity

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/FeedbackActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/FeedbackActivity.kt
@@ -3,6 +3,8 @@ package com.arnyminerz.escalaralcoiaicomtat.activity.isolated
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityFeedbackBinding
+import io.sentry.Sentry
+import io.sentry.UserFeedback
 
 class FeedbackActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedbackBinding
@@ -12,5 +14,23 @@ class FeedbackActivity : AppCompatActivity() {
         binding = ActivityFeedbackBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.sendFab.setOnClickListener {
+            with(binding) {
+                val name = nameEditText.text.toString()
+                val email = emailEditText.text.toString()
+                val message = messageEditText.text.toString()
+
+                val sentryId = Sentry.captureMessage(message)
+                val feedback = UserFeedback(sentryId).apply {
+                    this.comments = if (contactCheckBox.isChecked)
+                        "Don't contact me"
+                        else "I want to be contacted"
+                    this.email = email
+                    this.name = name
+                }
+                Sentry.captureUserFeedback(feedback)
+            }
+        }
     }
 }
