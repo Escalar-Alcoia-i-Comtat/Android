8c5a955ce8a99c1d13a8988b44238bf1959c108e
Arnau Mora <arnyminer.z@gmail.com>
2021-03-09 13:50:57 +0100

    :sparkles: Made `GeoStyle` Parcelable

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/GeoStyle.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/GeoStyle.kt
@@ -1,11 +1,12 @@
 package com.arnyminerz.escalaralcoiaicomtat.data.map
 
 import android.graphics.Color
+import android.os.Parcel
+import android.os.Parcelable
 import com.mapbox.mapboxsdk.plugins.annotation.FillOptions
 import com.mapbox.mapboxsdk.plugins.annotation.LineOptions
 import com.mapbox.mapboxsdk.style.layers.Property
 import com.mapbox.mapboxsdk.utils.ColorUtils.colorToRgbaString
-import java.io.Serializable
 
 const val LINE_WIDTH_MULTIPLIER = .5f
 
@@ -15,7 +16,14 @@ data class GeoStyle(
     val lineWidth: Float?,
     @Property.LINE_JOIN
     val lineJoin: String?
-) : Serializable {
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readFloat(),
+        parcel.readString()
+    )
+
     private fun colorIsNull(color: String?) =
         color == null || !color.startsWith("#") || color.contains("null")
 
@@ -30,6 +38,25 @@ data class GeoStyle(
     } catch (ex: NumberFormatException) {
         null
     } else null
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(fillColor)
+        dest.writeString(strokeColor)
+        lineWidth?.let { dest.writeFloat(it) }
+        dest.writeString(lineJoin)
+    }
+
+    companion object CREATOR : Parcelable.Creator<GeoStyle> {
+        override fun createFromParcel(parcel: Parcel): GeoStyle {
+            return GeoStyle(parcel)
+        }
+
+        override fun newArray(size: Int): Array<GeoStyle?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
 
 fun FillOptions.apply(geoStyle: GeoStyle): FillOptions {
