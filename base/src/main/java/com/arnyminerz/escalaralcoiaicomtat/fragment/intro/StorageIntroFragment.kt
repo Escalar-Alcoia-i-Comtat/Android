864048a9d9ccc1fd7a2a2b49ca55e9b721341cae
Arnau Mora <Arnyminer.Z@gmail.com>
2021-03-09 01:06:21 +0100

    :bug: Fixed small intro permissions bug from `intro-reason` patch (#87)

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/intro/StorageIntroFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/intro/StorageIntroFragment.kt
@@ -8,8 +8,8 @@ import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.arnyminerz.escalaralcoiaicomtat.R
-import com.arnyminerz.escalaralcoiaicomtat.activity.IntroActivity.Companion.hasStoragePermission
 import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentIntroStorageBinding
+import com.arnyminerz.escalaralcoiaicomtat.generic.isPermissionGranted
 
 @ExperimentalUnsignedTypes
 class StorageIntroFragment : Fragment() {
@@ -35,10 +35,10 @@ class StorageIntroFragment : Fragment() {
                     arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
                     STORAGE_PERMISSION_REQUEST
                 )
-                isEnabled = if(!hasStoragePermission(requireContext())) {
+                isEnabled = if (!requireContext().isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                     setText(R.string.status_permission_granted)
                     false
-                }else{
+                } else {
                     setText(R.string.action_grant_permission)
                     true
                 }
