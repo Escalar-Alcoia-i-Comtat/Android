9979445cd7ffb69961d337a2738fc8282430ccde
Arnau Mora <arnyminer.z@gmail.com>
2021-03-09 13:24:24 +0100

    :sparkles: Changed to icon size multiplier

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/GeoMarker.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/GeoMarker.kt
@@ -7,7 +7,6 @@ import android.os.Parcelable
 import com.arnyminerz.escalaralcoiaicomtat.data.preference.sharedPreferences
 import com.arnyminerz.escalaralcoiaicomtat.fragment.preferences.SETTINGS_MARKER_SIZE_PREF
 import com.arnyminerz.escalaralcoiaicomtat.generic.generateUUID
-import com.arnyminerz.escalaralcoiaicomtat.generic.mapFloat
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.Symbol
@@ -15,15 +14,12 @@ import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import timber.log.Timber
 
-private const val ICON_SIZE_IN_MIN = 1f
-private const val ICON_SIZE_IN_MAX = 5f
-private const val ICON_SIZE_OUT_MIN = .3f
-private const val ICON_SIZE_OUT_MAX = 1.2f
+private const val ICON_SIZE_MULTIPLIER = .4f
 
 @Suppress("unused")
 data class GeoMarker(
     val position: LatLng,
-    val iconSize: Float? = null,
+    val iconSizeMultiplier: Float = ICON_SIZE_MULTIPLIER,
     val windowData: MapObjectWindowData? = null
 ) : Parcelable {
     val id = generateUUID()
@@ -62,11 +58,7 @@ data class GeoMarker(
 
         if (iconImage != null) {
             Timber.d("Marker $id has an icon named $iconImage")
-            val iconSize = iconSize ?: mapFloat(
-                SETTINGS_MARKER_SIZE_PREF.get(context.sharedPreferences).toFloat(),
-                ICON_SIZE_IN_MIN, ICON_SIZE_IN_MAX,
-                ICON_SIZE_OUT_MIN, ICON_SIZE_OUT_MAX
-            )
+            val iconSize = SETTINGS_MARKER_SIZE_PREF.get(context.sharedPreferences) * iconSizeMultiplier
             symbolOptions = symbolOptions
                 .withIconImage(iconImage)
                 .withIconSize(iconSize)
@@ -83,7 +75,7 @@ data class GeoMarker(
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
         dest.writeParcelable(position, 0)
-        iconSize?.let { dest.writeFloat(it) }
+        dest.writeFloat(iconSizeMultiplier)
         if (windowData == null)
             dest.writeInt(0)
         else {
