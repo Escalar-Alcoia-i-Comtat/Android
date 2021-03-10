04ae28286f898d6c90e94b5033ff16f5d06510d4
Arnau Mora <arnyminer.z@gmail.com>
2021-03-10 12:03:12 +0100

    :sparkles: Reduced location min time to 10 seconds
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/MapConstants.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/MapConstants.kt
@@ -1,4 +1,4 @@
 package com.arnyminerz.escalaralcoiaicomtat.data.map
 
-const val LOCATION_UPDATE_MIN_TIME: Long = 1 * 60 * 1000 // 1 minute
+const val LOCATION_UPDATE_MIN_TIME: Long = 10 * 1000 // 10 seconds
 const val LOCATION_UPDATE_MIN_DIST: Float = 1.5f // 1.5 meters
