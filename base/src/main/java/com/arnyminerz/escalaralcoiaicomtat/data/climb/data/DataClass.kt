7a477d24273e96aa3661cc9f0610128081ffc5bb
Arnau Mora <arnyminer.z@gmail.com>
2021-03-14 00:45:32 +0100

    :sparkles: Added `count` and `countSync` methods
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/climb/data/DataClass.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/climb/data/DataClass.kt
@@ -159,6 +159,53 @@ fun <A : ParseObject> ParseQuery<A>.fetchPinOrNetworkSync(
     return list
 }
 
+/**
+ * Counts the amount of objects there are in a query
+ * @author Arnau Mora
+ * @since 20210314
+ * @param callback What to call when completed
+ * @return The loading task
+ * @throws NoInternetAccessException If no data is stored, and there's no Internet connection available
+ */
+@Throws(NoInternetAccessException::class)
+fun <A : ParseObject> ParseQuery<A>.count(
+    callback: ((count: Int, error: Exception?) -> Unit)? = null
+): Task<Int> = countInBackground().continueWithTask { task ->
+    val error = task.error
+    if (error != null) {
+        if (error is ParseException && error.code == ParseException.CACHE_MISS) {
+            Timber.w("No stored data found. Fetching from network.")
+            if (!appNetworkState.hasInternet) {
+                Timber.w("No Internet connection is available, and there's no stored data")
+                throw NoInternetAccessException("No Internet connection is available, and there's no stored data")
+            }
+            return@continueWithTask fromNetwork().countInBackground()
+        } else Timber.e(error, "Could not fetch data.")
+    }
+    return@continueWithTask task
+}.continueWithTask { task ->
+    callback?.invoke(task.result, task.error)
+    return@continueWithTask task
+}
+
+/**
+ * Counts the amount of objects there are in a query syncronously
+ * @author Arnau Mora
+ * @since 20210313
+ * @return The count
+ * @throws NoInternetAccessException If no data is stored, and there's no Internet connection available
+ */
+@Throws(NoInternetAccessException::class)
+@WorkerThread
+fun <A : ParseObject> ParseQuery<A>.countSync(): Int {
+    var result = -1
+    count { count, error ->
+        error?.let { throw it }
+        result = count
+    }.waitForCompletion()
+    return result
+}
+
 enum class DataClasses(val namespace: String) {
     AREA(Area.NAMESPACE),
     ZONE(Zone.NAMESPACE),
