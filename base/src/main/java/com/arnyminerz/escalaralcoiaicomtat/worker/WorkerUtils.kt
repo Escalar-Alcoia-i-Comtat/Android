d94747fb8afb82977c121d4543ee4512f9014f6b
Arnau Mora <arnyminer.z@gmail.com>
2021-03-13 23:25:25 +0100

    :sparkles: Added `WorkerUtils` for helping on `Worker`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/worker/WorkerUtils.kt
@@ -0,0 +1,23 @@
+package com.arnyminerz.escalaralcoiaicomtat.worker
+
+import androidx.collection.arrayMapOf
+import androidx.work.Data
+import androidx.work.ListenableWorker
+
+fun dataOf(vararg pairs: Pair<String, Any>): Data {
+    val map = arrayMapOf<String, Any>()
+    for ((key, value) in pairs)
+        map[key] = value
+    return dataOf(map)
+}
+
+fun dataOf(data: Map<String, Any>): Data {
+    val builder = Data.Builder()
+    builder.putAll(data)
+    return builder.build()
+}
+
+fun failure(error: String) =
+    ListenableWorker.Result.failure(dataOf(
+        "error" to error
+    ))
