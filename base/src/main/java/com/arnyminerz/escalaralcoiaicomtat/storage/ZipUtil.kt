f70fa162a07e120cc701eb883b489739a96eff65
Arnau Mora <arnyminer.z@gmail.com>
2021-03-18 21:06:21 +0100

    :sparkles: Added `ZipUtil`. Credit: [HailZeon](https://stackoverflow.com/users/2053024/hailzeon)
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/storage/ZipUtil.kt
@@ -0,0 +1,95 @@
+package com.arnyminerz.escalaralcoiaicomtat.storage
+
+import java.io.*
+import java.util.zip.ZipEntry
+import java.util.zip.ZipException
+import java.util.zip.ZipOutputStream
+
+private const val BUFFER = 2048
+
+/**
+ * Compresses a file into a zip file
+ * @author Arnau Mora
+ * @since 20210318
+ * @param file The source file
+ * @param target The target file
+ *
+ * @throws NullPointerException If the entry name is null
+ * @throws IllegalArgumentException If the entry name is longer than 0xFFFF byte
+ * @throws SecurityException If a security manager exists and its SecurityManager.checkRead(String)
+ * method denies read access to the file
+ * @throws ZipException If a ZIP format error has occurred
+ * @throws IOException If an I/O error has occurre
+ */
+@Throws(
+    NullPointerException::class,
+    IllegalArgumentException::class,
+    SecurityException::class,
+    ZipException::class,
+    IOException::class
+)
+fun zipFile(file: File, target: File) {
+    val origin: BufferedInputStream
+    val dest: FileOutputStream
+    var zipOutput: ZipOutputStream? = null
+    try {
+        dest = target.outputStream()
+        zipOutput = ZipOutputStream(dest.buffered(BUFFER))
+        if (file.isDirectory)
+            zipSubFolder(zipOutput, file, file.parent!!.length)
+        else {
+            val data = ByteArray(BUFFER)
+            val fi = file.inputStream()
+            origin = fi.buffered(BUFFER)
+            val entry = ZipEntry(getLastPathComponent(file.path))
+            entry.time = file.lastModified()
+            zipOutput.putNextEntry(entry)
+            var count = origin.read(data, 0, BUFFER)
+            while (count != -1) {
+                zipOutput.write(data, 0, count)
+                count = origin.read(data, 0, BUFFER)
+            }
+        }
+    } finally {
+        zipOutput?.close()
+    }
+}
+
+private fun zipSubFolder(zipOutput: ZipOutputStream, folder: File, basePathLength: Int) {
+    val files = folder.listFiles() ?: return
+    var origin: BufferedInputStream? = null
+    try {
+        for (file in files) {
+            if (file.isDirectory)
+                zipSubFolder(zipOutput, folder, basePathLength)
+            else {
+                val data = ByteArray(BUFFER)
+                val unmodifiedFilePath = file.path
+                val relativePath = unmodifiedFilePath.substring(basePathLength)
+                val fi = FileInputStream(unmodifiedFilePath)
+                origin = fi.buffered(BUFFER)
+                val entry = ZipEntry(relativePath)
+                entry.time = file.lastModified()
+                zipOutput.putNextEntry(entry)
+                var count = origin.read(data, 0, BUFFER)
+                while (count != -1) {
+                    zipOutput.write(data, 0, count)
+                    count = origin.read(data, 0, BUFFER)
+                }
+            }
+        }
+    } finally {
+        origin?.close()
+    }
+}
+
+/*
+ * gets the last path component
+ *
+ * Example: getLastPathComponent("downloads/example/fileToZip");
+ * Result: "fileToZip"
+ */
+private fun getLastPathComponent(filePath: String): String {
+    val segments = filePath.split("/").toTypedArray()
+    return if (segments.isEmpty()) "" else segments[segments.size - 1]
+}
