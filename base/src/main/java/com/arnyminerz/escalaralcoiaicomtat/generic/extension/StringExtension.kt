a4dbc9cc71a68e7c399be764a475759440d666a1
Arnau Mora <arnyminer.z@gmail.com>
2021-04-11 10:52:52 +0200

    :sparkles: Added `String?.toLatLng()`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/generic/extension/StringExtension.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/generic/extension/StringExtension.kt
@@ -1,8 +1,10 @@
 package com.arnyminerz.escalaralcoiaicomtat.generic.extension
 
+import com.mapbox.mapboxsdk.geometry.LatLng
 import java.text.ParseException
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Date
+import java.util.Locale
 
 val TIMESTAMP_FORMAT = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
 
@@ -22,4 +24,26 @@ fun String?.toTimestamp(): Date? =
         } catch (ex: ParseException) {
             null
         }
+    }
+
+/**
+ * Converts the [String] into a [LatLng].
+ * The format must be <latitude>,<longitude>
+ * @author Arnau Mora
+ * @since 20210411
+ * @return a new [LatLng], or null if [String] is null.
+ * @throws ParseException If there was an error while parsing the [LatLng]
+ * @throws NumberFormatException If the latitude or longitude are not a number
+ */
+fun String?.toLatLng(): LatLng? =
+    this?.replace(" ", "")?.let {
+        var comma = it.indexOf(',')
+        if (comma < 0)
+            throw ParseException(
+                "There is no \",\" in \"$it\". The format to follow is <latitude>,<longitude>.",
+                0
+            )
+        val latitude = it.substring(0, comma).toDouble()
+        val longitude = it.substring(++comma).toDouble()
+        LatLng(latitude, longitude)
     }
