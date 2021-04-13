039b6915dbd058a3256e151ada44b05ce027f46e
Arnau Mora <arnyminer.z@gmail.com>
2021-04-13 09:15:22 +0200

    :sparkles: Added `DownloadStatus.PARTIALLY`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/climb/dataclass/DownloadStatus.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/climb/dataclass/DownloadStatus.kt
@@ -1,11 +1,48 @@
 package com.arnyminerz.escalaralcoiaicomtat.data.climb.dataclass
 
+/**
+ * Specifies the download status of a [DataClass].
+ * @author Arnau Mora
+ * @since 20210413
+ */
 enum class DownloadStatus {
-    NOT_DOWNLOADED, DOWNLOADED, DOWNLOADING;
+    /**
+     * The [DataClass] is not downloaded.
+     * @author Arnau Mora
+     * @since 20210413
+     */
+    NOT_DOWNLOADED,
+
+    /**
+     * The [DataClass] is downloaded.
+     * @author Arnau Mora
+     * @since 20210413
+     */
+    DOWNLOADED,
+
+    /**
+     * The [DataClass] is currently being downloaded.
+     * @author Arnau Mora
+     * @since 20210413
+     */
+    DOWNLOADING,
+
+    /**
+     * The [DataClass] is not completely downloaded, this means that maybe a child or more is
+     * downloaded, but the whole class isn't.
+     * @author Arnau Mora
+     * @since 20210413
+     */
+    PARTIALLY;
 
     override fun toString(): String = name
 
     operator fun not(): Boolean = this != DOWNLOADED
 
+    /**
+     * Checks if the [DataClass] is completely downloaded.
+     * @author Arnau Mora
+     * @since 20210413
+     */
     fun isDownloaded(): Boolean = this == DOWNLOADED
 }
