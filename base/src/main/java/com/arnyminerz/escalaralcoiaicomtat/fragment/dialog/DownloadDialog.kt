d2e7c2eda064531c0d9c9a66c8d0734bfda2f534
Arnau Mora <arnyminer.z@gmail.com>
2021-04-16 21:44:48 +0200

    :sparkles: Added partial download
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/dialog/DownloadDialog.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/dialog/DownloadDialog.kt
@@ -15,62 +15,84 @@ import kotlinx.coroutines.flow.toCollection
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
-class DownloadDialog(
+class DownloadDialog<T : DataClass<*, *>>(
     private val context: Context,
-    private val data: DataClass<*, *>,
-    private val firestore: FirebaseFirestore
+    private val data: T,
+    private val firestore: FirebaseFirestore,
+    private val partialDownload: Boolean,
+    private val downloader: (dataClass: T) -> Unit,
 ) {
     fun show(deleteCallback: (() -> Unit)? = null) {
-        val date = data.downloadDate(context)
-        MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_App_MaterialAlertDialog)
-            .setTitle(data.displayName)
-            .setMessage(
-                context.getString(
-                    R.string.dialog_downloaded_msg,
-                    date?.let {
-                        android.text.format.DateFormat.format(
-                            context.getString(R.string.date_format),
-                            date
-                        )
-                    } ?: "N/A"
-                ) + '\n' + context.getString(
-                    R.string.dialog_uses_storage_msg,
-                    runBlocking { humanReadableByteCountBin(data.size(context)) }
-                )
-            )
-            .setNeutralButton(R.string.action_delete) { dialog, _ ->
-                dialog.dismiss()
-                MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_App_MaterialAlertDialog)
-                    .setTitle(R.string.downloads_delete_dialog_title)
-                    .setMessage(
-                        context.getString(
-                            R.string.downloads_delete_dialog_msg,
-                            data.displayName
-                        )
+        var builder =
+            MaterialAlertDialogBuilder(context, R.style.ThemeOverlay_App_MaterialAlertDialog)
+                .setTitle(data.displayName)
+        if (partialDownload) {
+            builder = builder
+                .setMessage(
+                    context.getString(
+                        R.string.dialog_downloaded_partially_msg,
+                        data.displayName
                     )
-                    .setPositiveButton(R.string.action_delete) { d, _ ->
-                        d.dismiss()
-                        when (data) {
-                            is Area, is Zone, is Sector ->
-                                doAsync {
-                                    val children = arrayListOf<DataClassImpl>()
-                                    data.getChildren(firestore).toCollection(children)
-                                    for (child in children)
-                                        if (child is DataClass<*, *>)
-                                            child.delete(context)
+                )
+                .setPositiveButton(R.string.action_download) { dialog, _ ->
+                    downloader(data)
+                    dialog.dismiss()
+                }
+        } else {
+            val date = data.downloadDate(context)
 
-                                    data.delete(context)
-                                    deleteCallback?.invoke()
-                                }
-                            else -> Timber.e("Data is not valid.")
+            val message = context.getString(
+                R.string.dialog_downloaded_msg,
+                date?.let {
+                    android.text.format.DateFormat.format(
+                        context.getString(R.string.date_format),
+                        date
+                    )
+                } ?: "N/A"
+            ) + '\n' + context.getString(
+                R.string.dialog_uses_storage_msg,
+                runBlocking { humanReadableByteCountBin(data.size(context)) }
+            )
+
+            builder = builder
+                .setMessage(message)
+                .setNeutralButton(R.string.action_delete) { dialog, _ ->
+                    dialog.dismiss()
+                    MaterialAlertDialogBuilder(
+                        context,
+                        R.style.ThemeOverlay_App_MaterialAlertDialog
+                    )
+                        .setTitle(R.string.downloads_delete_dialog_title)
+                        .setMessage(
+                            context.getString(
+                                R.string.downloads_delete_dialog_msg,
+                                data.displayName
+                            )
+                        )
+                        .setPositiveButton(R.string.action_delete) { d, _ ->
+                            d.dismiss()
+                            when (data) {
+                                is Area, is Zone, is Sector ->
+                                    doAsync {
+                                        val children = arrayListOf<DataClassImpl>()
+                                        data.getChildren(firestore).toCollection(children)
+                                        for (child in children)
+                                            if (child is DataClass<*, *>)
+                                                child.delete(context)
+
+                                        data.delete(context)
+                                        deleteCallback?.invoke()
+                                    }
+                                else -> Timber.e("Data is not valid.")
+                            }
                         }
-                    }
-                    .setNegativeButton(R.string.action_cancel) { d, _ ->
-                        d.dismiss()
-                    }
-                    .show()
-            }
-            .setNegativeButton(R.string.action_close, null)
-            .show()
+                        .setNegativeButton(R.string.action_cancel) { d, _ ->
+                            d.dismiss()
+                        }
+                        .show()
+                }
+                .setNegativeButton(R.string.action_close, null)
+        }
+        builder.show()
     }
 }
