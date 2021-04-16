dfeaeb43f340ed47bbaa60402d92955db8a350fc
Arnau Mora <arnyminer.z@gmail.com>
2021-04-16 17:33:17 +0200

    :fire: Removed `runBlocking`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/connection/web/WebUtils.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/connection/web/WebUtils.kt
@@ -1,15 +1,13 @@
 package com.arnyminerz.escalaralcoiaicomtat.connection.web
 
-import kotlinx.coroutines.runBlocking
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
 
-fun download(src: String): InputStream =
-    runBlocking {
-        val url = URL(src)
-        val connection = url.openConnection() as HttpURLConnection
-        connection.doInput = true
-        connection.connect()
-        connection.inputStream
-    }
+fun download(src: String): InputStream {
+    val url = URL(src)
+    val connection = url.openConnection() as HttpURLConnection
+    connection.doInput = true
+    connection.connect()
+    return connection.inputStream
+}
