8ac9f5fdabf9a79a8b88248d8ceb93fc9d3dd9d0
Arnau Mora <arnyminer.z@gmail.com>
2021-04-22 18:56:28 +0200

    :sparkles: Added custom Timber tree for Crashlytics logging.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/shared/App.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/shared/App.kt
@@ -2,11 +2,17 @@ package com.arnyminerz.escalaralcoiaicomtat.shared
 
 import android.app.Application
 import android.content.Context
+import android.util.Log
+import com.arnyminerz.escalaralcoiaicomtat.BuildConfig
 import com.arnyminerz.escalaralcoiaicomtat.network.base.ConnectivityProvider
+import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
+
+private const val CRASHLYTICS_KEY_PRIORITY = "priority"
+private const val CRASHLYTICS_KEY_TAG = "tag"
 
 class App : Application(), ConnectivityProvider.ConnectivityStateListener {
     private val provider: ConnectivityProvider
@@ -16,6 +22,9 @@ class App : Application(), ConnectivityProvider.ConnectivityStateListener {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(if (BuildConfig.DEBUG) Timber.DebugTree() else CrashReportingTree())
+        Timber.v("Planted Timber.")
 
         sharedPreferences =
             applicationContext.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
@@ -48,5 +57,22 @@ class App : Application(), ConnectivityProvider.ConnectivityStateListener {
 
     override suspend fun onStateChangeAsync(state: ConnectivityProvider.NetworkState) {
         Timber.v("Network state updated asyncronously: $state")
+    }
+
+    private inner class CrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            val crashlytics = Firebase.crashlytics
+
+            crashlytics.setCustomKey(CRASHLYTICS_KEY_PRIORITY, priority)
+            if (tag != null)
+                crashlytics.setCustomKey(CRASHLYTICS_KEY_TAG, tag)
+
+            if (priority == Log.DEBUG || priority == Log.VERBOSE || priority == Log.INFO || priority == Log.WARN)
+                crashlytics.log(message)
+            else if (t != null)
+                crashlytics.recordException(t)
+            else
+                crashlytics.log(message)
+        }
     }
 }
