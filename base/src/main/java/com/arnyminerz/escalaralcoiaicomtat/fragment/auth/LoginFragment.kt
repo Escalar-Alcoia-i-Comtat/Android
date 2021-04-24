dcfd0570d74e910cb5022df2b8d3acd17813b310
Arnau Mora <arnyminer.z@gmail.com>
2021-04-24 17:34:11 +0200

    :sparkles: Added fields editor actions and login focus clear.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/auth/LoginFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/auth/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentLoginBinding
+import com.arnyminerz.escalaralcoiaicomtat.list.viewListOf
 
 class LoginFragment private constructor() : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -24,6 +25,27 @@ class LoginFragment private constructor() : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.emailEditText.setOnEditorActionListener { _, _, _ ->
+            binding.passwordEditText.requestFocus()
+
+            true
+        }
+
+        binding.passwordEditText.setOnEditorActionListener { _, _, _ ->
+            binding.loginButton.performClick()
+
+            true
+        }
+
+        binding.loginButton.setOnClickListener {
+            val fields = viewListOf(binding.emailEditText, binding.passwordEditText)
+            fields.clearFocus()
+        }
     }
 
     companion object {
