8b74ffc2aa1d6862152c6ec3145bb1851ed1db93
Arnau Mora <arnyminer.z@gmail.com>
2021-04-24 17:57:34 +0200

    :sparkles: Added login error handlers.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/auth/LoginFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/auth/LoginFragment.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.StringRes
 import androidx.fragment.app.Fragment
+import com.arnyminerz.escalaralcoiaicomtat.R
 import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentLoginBinding
+import com.arnyminerz.escalaralcoiaicomtat.generic.toast
 import com.arnyminerz.escalaralcoiaicomtat.list.viewListOf
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.FirebaseError
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
 class LoginFragment private constructor() : Fragment() {
     private var _binding: FragmentLoginBinding? = null
@@ -55,9 +64,59 @@ class LoginFragment private constructor() : Fragment() {
         }
 
         binding.loginButton.setOnClickListener {
-            val fields = viewListOf(binding.emailEditText, binding.passwordEditText)
+            val fields = viewListOf(
+                binding.emailEditText,
+                binding.passwordEditText,
+                binding.loginButton,
+                binding.registerButton
+            )
             fields.clearFocus()
+            fields.disable()
+
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener { result ->
+
+                }
+                .addOnFailureListener { exception ->
+                    Timber.w(exception, "Could not login.")
+                    val e = exception as FirebaseAuthException
+                    when (e.errorCode.toInt()) {
+                        FirebaseError.ERROR_USER_NOT_FOUND ->
+                            showError(
+                                binding.emailTextField,
+                                R.string.login_error_user_not_found
+                            )
+                        FirebaseError.ERROR_INVALID_CREDENTIAL ->
+                            showError(
+                                binding.passwordTextField,
+                                R.string.login_error_invalid_credentials
+                            )
+                        FirebaseError.ERROR_USER_DISABLED ->
+                            showError(
+                                binding.emailTextField,
+                                R.string.login_error_user_disabled
+                            )
+                        else -> toast(context, R.string.toast_error_internal)
+                    }
+                }
+                .addOnCompleteListener {
+                    fields.enable()
+                }
         }
+    }
+
+    /**
+     * Shows an error to the user through a text field.
+     * @author Arnau Mora
+     * @since 20210425
+     * @param field The field to update
+     * @param error The string resource of the message to show
+     */
+    private fun showError(field: TextInputLayout, @StringRes error: Int) {
+        field.isErrorEnabled = true
+        field.error = getString(error)
     }
 
     companion object {
