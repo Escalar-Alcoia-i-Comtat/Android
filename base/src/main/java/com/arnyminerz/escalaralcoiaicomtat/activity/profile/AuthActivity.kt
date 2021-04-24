0902fe9adbaa12b8d4a4a9fc2a3e6f3812d6541a
Arnau Mora <arnyminer.z@gmail.com>
2021-04-24 18:03:58 +0200

    :memo: Added Activity javadoc

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/profile/AuthActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/profile/AuthActivity.kt
@@ -6,7 +6,16 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.arnyminerz.escalaralcoiaicomtat.activity.model.LanguageFragmentActivity
 import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityAuthBinding
 import com.arnyminerz.escalaralcoiaicomtat.fragment.auth.LoginFragment
+import com.arnyminerz.escalaralcoiaicomtat.shared.LOGGED_IN_REQUEST_CODE
 
+/**
+ * Allows the user to get logged into the app, register, or get some help on a lost account.
+ * Note that this returns a result once an action has been completed. See the also see section of
+ * the javadoc.
+ * @author Arnau Mora
+ * @since 20210425
+ * @see LOGGED_IN_REQUEST_CODE
+ */
 class AuthActivity : LanguageFragmentActivity() {
     private lateinit var binding: ActivityAuthBinding
 
