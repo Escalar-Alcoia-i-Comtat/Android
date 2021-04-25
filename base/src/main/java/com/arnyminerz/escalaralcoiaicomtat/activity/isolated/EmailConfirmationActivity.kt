97662a428fb91a0dd79e9346d3747d17fd4c61c3
Arnau Mora <arnyminer.z@gmail.com>
2021-04-25 16:14:37 +0200

    :sparkles: Added handler for removed accounts.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/EmailConfirmationActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/EmailConfirmationActivity.kt
@@ -1,12 +1,18 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity.isolated
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.arnyminerz.escalaralcoiaicomtat.activity.LoadingActivity
 import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityEmailConfirmationBinding
 import com.arnyminerz.escalaralcoiaicomtat.fragment.preferences.PREF_WAITING_EMAIL_CONFIRMATION
+import com.arnyminerz.escalaralcoiaicomtat.generic.awaitTask
+import com.arnyminerz.escalaralcoiaicomtat.generic.doAsync
+import com.arnyminerz.escalaralcoiaicomtat.generic.uiContext
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
 import com.google.firebase.ktx.Firebase
 import timber.log.Timber
 
@@ -22,19 +28,32 @@ class EmailConfirmationActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
-        Timber.v("Getting currently logged in user...")
-        val user = Firebase.auth.currentUser
-        if (user == null) {
-            Timber.v("There's no logged in user, loading app...")
-            PREF_WAITING_EMAIL_CONFIRMATION.put(false)
-            startActivity(Intent(this, LoadingActivity::class.java))
-        } else {
-            Timber.v("Checking if verified")
-            val verified = user.isEmailVerified
-            if (verified) {
-                Timber.v("The user's email has been verified, loading app...")
-                PREF_WAITING_EMAIL_CONFIRMATION.put(false)
-                startActivity(Intent(this, LoadingActivity::class.java))
+        doAsync {
+            Timber.v("Getting currently logged in user...")
+            val user = Firebase.auth.currentUser
+            try {
+                user?.reload()?.awaitTask()
+            } catch (_: FirebaseAuthInvalidUserException) {
+                uiContext {
+                    Timber.w("The user has been removed, loading app...")
+                    PREF_WAITING_EMAIL_CONFIRMATION.put(false)
+                    startActivity(Intent(this, LoadingActivity::class.java))
+                }
+            }
+            uiContext {
+                if (user == null) {
+                    Timber.v("There's no logged in user, loading app...")
+                    PREF_WAITING_EMAIL_CONFIRMATION.put(false)
+                    startActivity(Intent(this, LoadingActivity::class.java))
+                } else {
+                    Timber.v("Checking if verified")
+                    val verified = user.isEmailVerified
+                    if (verified) {
+                        Timber.v("The user's email has been verified, loading app...")
+                        PREF_WAITING_EMAIL_CONFIRMATION.put(false)
+                        startActivity(Intent(this, LoadingActivity::class.java))
+                    }
+                }
             }
         }
     }
