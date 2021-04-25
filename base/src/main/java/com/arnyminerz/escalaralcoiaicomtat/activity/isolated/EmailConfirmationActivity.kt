801997841e85782bffdbe9e581165d118f423869
Arnau Mora <arnyminer.z@gmail.com>
2021-04-25 14:30:28 +0200

    :sparkles: Added redirection when email has been confirmed.

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/EmailConfirmationActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/EmailConfirmationActivity.kt
@@ -1,8 +1,14 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity.isolated
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.arnyminerz.escalaralcoiaicomtat.activity.LoadingActivity
 import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityEmailConfirmationBinding
+import com.arnyminerz.escalaralcoiaicomtat.fragment.preferences.PREF_WAITING_EMAIL_CONFIRMATION
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
 class EmailConfirmationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEmailConfirmationBinding
@@ -11,5 +17,25 @@ class EmailConfirmationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEmailConfirmationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Timber.v("Getting currently logged in user...")
+        val user = Firebase.auth.currentUser
+        if (user == null) {
+            Timber.v("There's no logged in user, loading app...")
+            PREF_WAITING_EMAIL_CONFIRMATION.put(false)
+            startActivity(Intent(this, LoadingActivity::class.java))
+        } else {
+            Timber.v("Checking if verified")
+            val verified = user.isEmailVerified
+            if (verified) {
+                Timber.v("The user's email has been verified, loading app...")
+                PREF_WAITING_EMAIL_CONFIRMATION.put(false)
+                startActivity(Intent(this, LoadingActivity::class.java))
+            }
+        }
     }
 }
