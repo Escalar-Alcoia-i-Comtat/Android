bda972c3918d9d0e4aa0d6ff5710e498c42c1cec
Arnau Mora <arnyminer.z@gmail.com>
2021-04-30 12:53:15 +0200

    :sparkles: Added `setProfileImage` and firestore reference updates
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/auth/ProfileUtils.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/auth/ProfileUtils.kt
@@ -1,6 +1,7 @@
 package com.arnyminerz.escalaralcoiaicomtat.auth
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
@@ -32,22 +33,44 @@ import kotlin.coroutines.suspendCoroutine
  * @author Arnau Mora
  * @since 20210424
  * @param context The context where the function is getting called.
+ * @param firestore The Firestore instance to update references.
  * @param user The user to update.
  * @param progressListener This will notify you about the image upload progress.
  */
 @WorkerThread
 suspend fun setDefaultProfileImage(
     context: Context,
+    firestore: FirebaseFirestore,
     user: FirebaseUser,
+    progressListener: ((progress: ValueMax<Long>) -> Unit)?
+): UploadTask.TaskSnapshot = setProfileImage(
+    firestore,
+    user,
+    ContextCompat.getDrawable(context, R.drawable.ic_profile_image)!!.toBitmap(),
+    progressListener
+)
+
+/**
+ * Uploads and updates the user's profile image.
+ * @author Arnau Mora
+ * @since 20210424
+ * @param firestore The Firestore instance to update references.
+ * @param user The user to update.
+ * @param image The image to use
+ * @param progressListener This will notify you about the image upload progress.
+ */
+@WorkerThread
+suspend fun setProfileImage(
+    firestore: FirebaseFirestore,
+    user: FirebaseUser,
+    image: Bitmap,
     progressListener: ((progress: ValueMax<Long>) -> Unit)?
 ): UploadTask.TaskSnapshot = suspendCoroutine { cont ->
     try {
-        Timber.v("Registration has been successful, setting default profile image...")
         Timber.v("Starting profile image upload...")
         val storageRef = Firebase.storage.reference
         val profileImageRef = storageRef.child("users/${user.uid}/profile.webp")
-        val d = ContextCompat.getDrawable(context, R.drawable.ic_profile_image)
-        val profileImage = d!!.toBitmap()
+        val profileImage = image
             .cropToSquare()
             ?.scale(PROFILE_IMAGE_SIZE.toInt(), PROFILE_IMAGE_SIZE.toInt())
         val baos = ByteArrayOutputStream()
@@ -66,6 +89,7 @@ suspend fun setDefaultProfileImage(
                 doAsync {
                     try {
                         updateProfileImage(
+                            firestore,
                             user,
                             "gs://escalaralcoiaicomtat.appspot.com" + profileImageRef.path
                         )
@@ -90,11 +114,16 @@ suspend fun setDefaultProfileImage(
  * Updates the user's profile image address.
  * @author Arnau Mora
  * @since 20210425
+ * @param firestore The Firestore instance to update references.
  * @param user The user to update.
  * @param imageDownloadUrl The uploaded image url.
  */
 @UiThread
-suspend fun updateProfileImage(user: FirebaseUser, imageDownloadUrl: String) =
+suspend fun updateProfileImage(
+    firestore: FirebaseFirestore,
+    user: FirebaseUser,
+    imageDownloadUrl: String
+) =
     suspendCoroutine<Void> { cont ->
         Timber.v("Processing uri...")
         val profileImageUri = Uri.parse(imageDownloadUrl)
@@ -106,16 +135,23 @@ suspend fun updateProfileImage(user: FirebaseUser, imageDownloadUrl: String) =
             }
         Timber.v("Submitting change request...")
         user.updateProfile(changeRequest)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // TODO: Update Firestore reference
-                    Timber.v("Updated profile image successfully.")
-                    cont.resume(task.result)
-                } else {
-                    val e = task.exception!!
-                    Timber.e(e, "Could not update profile image.")
-                    cont.resumeWithException(e)
-                }
+            .addOnSuccessListener {
+                Timber.v("Updated profile image successfully. Updating Firestore reference...")
+                firestore.collection("Users")
+                    .document(user.uid)
+                    .update("profileImage", imageDownloadUrl)
+                    .addOnSuccessListener {
+                        Timber.v("Updated reference! Resuming...")
+                        cont.resume(it)
+                    }
+                    .addOnFailureListener {
+                        Timber.e(it, "Could not update reference.")
+                        cont.resumeWithException(it)
+                    }
+            }
+            .addOnFailureListener {
+                Timber.e(it, "Could not update profile image.")
+                cont.resumeWithException(it)
             }
     }
 
@@ -123,6 +159,7 @@ suspend fun updateProfileImage(user: FirebaseUser, imageDownloadUrl: String) =
  * Updates the user's profile image address.
  * @author Arnau Mora
  * @since 20210425
+ * @param firestore The Firestore instance to update references.
  * @param user The user to update.
  * @param displayName The new display name of the user.
  * @throws FirebaseAuthInvalidUserException Thrown if the current user's account has been disabled,
@@ -130,14 +167,28 @@ suspend fun updateProfileImage(user: FirebaseUser, imageDownloadUrl: String) =
  */
 @UiThread
 @Throws(FirebaseAuthInvalidUserException::class)
-suspend fun updateDisplayName(user: FirebaseUser, displayName: String) =
+suspend fun updateDisplayName(
+    firestore: FirebaseFirestore,
+    user: FirebaseUser,
+    displayName: String
+) =
     suspendCoroutine<Void> { cont ->
         user.updateProfile(
             userProfileChangeRequest {
                 this.displayName = displayName
             }
         ).addOnSuccessListener {
-            cont.resume(it)
+            firestore.collection("Users")
+                .document(user.uid)
+                .update("displayName", displayName)
+                .addOnSuccessListener { result ->
+                    Timber.v("Updated reference! Resuming...")
+                    cont.resume(result)
+                }
+                .addOnFailureListener { e ->
+                    Timber.e(e, "Could not update reference.")
+                    cont.resumeWithException(e)
+                }
         }.addOnFailureListener {
             Timber.e(it, "Could not update profile data.")
             cont.resumeWithException(it)
@@ -149,6 +200,8 @@ suspend fun updateDisplayName(user: FirebaseUser, displayName: String) =
  * name and profile image.
  * @author Arnau Mora
  * @since 20210430
+ * @param firestore The Firestore instance to update references.
+ * @param user The user to store the reference of
  */
 @UiThread
 suspend fun createFirestoreUserReference(firestore: FirebaseFirestore, user: FirebaseUser) =
