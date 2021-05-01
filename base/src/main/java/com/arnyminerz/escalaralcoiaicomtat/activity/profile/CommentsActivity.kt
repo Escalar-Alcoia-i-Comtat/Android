97615bc51f1655223b293b484df571def59068c0
Arnau Mora <arnyminer.z@gmail.com>
2021-05-01 13:04:42 +0200

    :sparkles: Added comments loading
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/profile/CommentsActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/profile/CommentsActivity.kt
@@ -1,13 +1,17 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity.profile
 
 import android.os.Bundle
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.arnyminerz.escalaralcoiaicomtat.data.climb.path.MarkedDataInt
 import com.arnyminerz.escalaralcoiaicomtat.data.climb.path.Path
 import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityCommentsBinding
 import com.arnyminerz.escalaralcoiaicomtat.generic.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.generic.finishActivityWithResult
 import com.arnyminerz.escalaralcoiaicomtat.generic.getExtra
+import com.arnyminerz.escalaralcoiaicomtat.generic.uiContext
+import com.arnyminerz.escalaralcoiaicomtat.list.completions.adapter.CommentsAdapter
 import com.arnyminerz.escalaralcoiaicomtat.shared.EXTRA_PATH_DOCUMENT
 import com.arnyminerz.escalaralcoiaicomtat.shared.RESULT_CODE_MISSING_DATA
 import com.arnyminerz.escalaralcoiaicomtat.shared.RESULT_CODE_UNKNOWN_ERROR
@@ -31,7 +35,12 @@ class CommentsActivity : AppCompatActivity() {
 
     private lateinit var firestore: FirebaseFirestore
 
-    val completions = arrayListOf<MarkedDataInt>()
+    /**
+     * Stores the completions that people have marked in the loaded path.
+     * @author Arnau Mora
+     * @since 20210501
+     */
+    private val completions = arrayListOf<MarkedDataInt>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,12 +71,45 @@ class CommentsActivity : AppCompatActivity() {
 
                 doAsync {
                     Timber.v("Loaded path data, loading completions...")
+                    completions.clear()
                     path.getCompletions(firestore).toCollection(completions)
+                    uiContext {
+                        loadLists()
+                    }
                 }
             }
             .addOnFailureListener {
                 Timber.e(it, "Could not get path data")
                 finishActivityWithResult(RESULT_CODE_UNKNOWN_ERROR, null)
             }
+    }
+
+    /**
+     * Loads the notes and comments recycler views with the contents loaded.
+     * @author Arnau Mora
+     * @since 20210501
+     * @see completions
+     */
+    @UiThread
+    private fun loadLists() {
+        if (completions.isEmpty()) {
+            Timber.w("Cannot load lists since completions is empty.")
+            return
+        }
+        Timber.v("Processing completions to get comments and notes...")
+        val comments = arrayListOf<MarkedDataInt>()
+        val notes = arrayListOf<MarkedDataInt>()
+        for (completion in completions) {
+            if (completion.comment != null && completion.comment.isNotBlank())
+                comments.add(completion)
+            if (completion.notes != null && completion.notes.isNotBlank())
+                notes.add(completion)
+        }
+
+        Timber.v("Preparing comments recycler view's adapter.")
+        val commentsAdapter = CommentsAdapter(this, completions)
+        Timber.v("Setting comments recycler view's adapter and layout.")
+        binding.commentsRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.commentsRecyclerView.adapter = commentsAdapter
     }
 }
