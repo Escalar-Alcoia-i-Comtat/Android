c4ed8970946827dd6659a98b130d5dab8b57881d
Arnau Mora <arnyminer.z@gmail.com>
2021-05-01 12:23:16 +0200

    :sparkles: Added `CommentsViewHolder`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/list/completions/holder/CommentsViewHolder.kt
@@ -0,0 +1,16 @@
+package com.arnyminerz.escalaralcoiaicomtat.list.completions.holder
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.arnyminerz.escalaralcoiaicomtat.R
+
+class CommentsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val profileImageView: ImageView = view.findViewById(R.id.profileImage_imageView)
+
+    val profileNameTextView: TextView = view.findViewById(R.id.profileName_textView)
+    val gradeTextView: TextView = view.findViewById(R.id.grade_textView)
+    val commentTextView: TextView = view.findViewById(R.id.comment_textView)
+    val likesTextView: TextView = view.findViewById(R.id.likes_textView)
+}
