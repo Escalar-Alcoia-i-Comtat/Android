318778aacb1f8efba8909f017a976a31b65a0ff2
Arnau Mora <arnyminer.z@gmail.com>
2021-05-02 15:55:14 +0200

    :sparkles: Added `CompletionType`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/climb/path/CompletionType.kt
@@ -0,0 +1,12 @@
+package com.arnyminerz.escalaralcoiaicomtat.data.climb.path
+
+/**
+ * Sets the type of completion the user made of a path.
+ * @author Arnau Mora
+ * @since 20210502
+ */
+enum class CompletionType {
+    FIRST,
+    TOP_ROPE,
+    LEAD
+}
