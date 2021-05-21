a47207a39f14d2b1400445655aa94f6599b3cdc9
Arnau Mora <arnyminer.z@gmail.com>
2021-05-21 12:21:58 +0200

    :sparkles: Added `CouldNotCreateDynamicLinkException`

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/exception/CouldNotCreateDynamicLinkException.kt
@@ -0,0 +1,5 @@
+package com.arnyminerz.escalaralcoiaicomtat.exception
+
+import com.google.firebase.FirebaseException
+
+class CouldNotCreateDynamicLinkException(msg: String) : FirebaseException(msg)
