2d78b0f19adcd714455807c07a0bbff829e419ca
Arnau Mora <arnyminer.z@gmail.com>
2021-06-04 16:46:13 +0200

    :sparkles: Added `MARKER_SIZE`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/MapConstants.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/data/map/MapConstants.kt
@@ -17,6 +17,13 @@ const val DEFAULT_LATITUDE = 38.7284401
 const val DEFAULT_LONGITUDE = -0.43821
 const val DEFAULT_ZOOM = 12.0f
 
+/**
+ * The size of the images from the map.
+ * @author Arnau Mora
+ * @since 20210604
+ * @see GeoIcon
+ */
+const val MARKER_SIZE = 56
 const val SMALL_ICON_SIZE_MULTIPLIER = .35f
 const val LINE_WIDTH_MULTIPLIER = 1f
 const val MAP_LOAD_PADDING = 50
