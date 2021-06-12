a179ff21e7494c1c70ab4b92c920aac1440b9518
Arnau Mora <arnyminer.z@gmail.com>
2021-06-12 17:45:25 +0200

    :sparkles: Simplified `ImageLoadParameters`
    Signed-off-by: Arnau Mora Gras <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/view/ImageLoadParameters.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/view/ImageLoadParameters.kt
@@ -1,85 +1,11 @@
 package com.arnyminerz.escalaralcoiaicomtat.view
 
-import com.bumptech.glide.RequestBuilder
-import com.bumptech.glide.TransitionOptions
-import com.bumptech.glide.request.RequestOptions
-import timber.log.Timber
+import android.widget.ImageView
 
-/**
- * Applies the data from [imageLoadParameters] into the request builder
- * @author Arnau Mora
- * @since 20210323
- * @param imageLoadParameters The parameters to set
- * @return The [RequestBuilder] instance for chaining calls
- * @see RequestBuilder
- * @see ImageLoadParameters
- */
-fun <T> RequestBuilder<T>.apply(imageLoadParameters: ImageLoadParameters<T>?): RequestBuilder<T> {
-    imageLoadParameters?.let { ilp ->
-        ilp.requestOptions?.let {
-            apply(it)
-            Timber.v("Applied request options")
-        }
-        ilp.transitionOptions?.let {
-            transition(it)
-            Timber.v("Applied transition options")
-        }
-        ilp.thumbnailSize?.let {
-            thumbnail(it)
-            Timber.v("Applied thumbnail size options")
-        }
-        if (!ilp.showPlaceholder) {
-            Timber.v("Disabling placeholder")
-            return this.placeholder(null)
-        }
-    }
-    return this
-}
-
-class ImageLoadParameters<T> {
-    internal var requestOptions: RequestOptions? = null
-    internal var transitionOptions: TransitionOptions<*, T>? = null
-    internal var thumbnailSize: Float? = null
+class ImageLoadParameters {
     internal var showPlaceholder: Boolean = true
     var resultImageScale: Float? = null
-
-    /**
-     * Sets request options for loading the image
-     * @author Arnau Mora
-     * @since 20210323
-     * @param requestOptions The options
-     * @return The current instance for chaining calls
-     * @see RequestOptions
-     */
-    fun withRequestOptions(requestOptions: RequestOptions?): ImageLoadParameters<T> {
-        this.requestOptions = requestOptions
-        return this
-    }
-
-    /**
-     * Sets transition options for loading the image
-     * @author Arnau Mora
-     * @since 20210323
-     * @param transitionOptions The options to apply
-     * @return The current instance for chaining calls
-     * @see TransitionOptions
-     */
-    fun withTransitionOptions(transitionOptions: TransitionOptions<*, T>?): ImageLoadParameters<T> {
-        this.transitionOptions = transitionOptions
-        return this
-    }
-
-    /**
-     * Sets the scale of the image while it's getting loaded
-     * @author Arnau Mora
-     * @since 20210323
-     * @param thumbnailSize The size of the image while it's getting loaded
-     * @return The current instance for chaining calls
-     */
-    fun withThumbnailSize(thumbnailSize: Float?): ImageLoadParameters<T> {
-        this.thumbnailSize = thumbnailSize
-        return this
-    }
+    var scaleType: ImageView.ScaleType = ImageView.ScaleType.CENTER_CROP
 
     /**
      * Sets the image scale once the load has been completed
@@ -88,8 +14,20 @@ class ImageLoadParameters<T> {
      * @param scale The image's scale
      * @return The current instance for chaining calls
      */
-    fun withResultImageScale(scale: Float?): ImageLoadParameters<T> {
+    fun withResultImageScale(scale: Float?): ImageLoadParameters {
         this.resultImageScale = scale
+        return this
+    }
+
+    /**
+     * Sets the image resize mode.
+     * @author Arnau Mora
+     * @since 20210612
+     * @param scaleType The [ImageView.ScaleType] to set.
+     * @return The current instance for chaining calls
+     */
+    fun withScaleType(scaleType: ImageView.ScaleType): ImageLoadParameters {
+        this.scaleType = scaleType
         return this
     }
 
@@ -100,7 +38,7 @@ class ImageLoadParameters<T> {
      * @param showPlaceholder If the placeholder should be shown
      * @return The current instance for chaining calls
      */
-    fun setShowPlaceholder(showPlaceholder: Boolean): ImageLoadParameters<T> {
+    fun setShowPlaceholder(showPlaceholder: Boolean): ImageLoadParameters {
         this.showPlaceholder = showPlaceholder
         return this
     }
