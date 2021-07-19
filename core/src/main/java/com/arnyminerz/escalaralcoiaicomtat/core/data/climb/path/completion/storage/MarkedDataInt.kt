ef8b94c8eee802a171c4a0cc4b3f00f509b048cd
Arnau Mora <arnyminer.z@gmail.com>
2021-07-19 13:03:18 +0200

    :sparkles: Added `MarkedDataInt.newInstance`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/path/completion/storage/MarkedDataInt.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/path/completion/storage/MarkedDataInt.kt
@@ -1,10 +1,15 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.completion.storage
 
+import com.arnyminerz.escalaralcoiaicomtat.core.data.auth.User
 import com.arnyminerz.escalaralcoiaicomtat.core.data.auth.VisibleUserData
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.completion.CompletionType
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.functions.FirebaseFunctionsException
+import timber.log.Timber
 
 /**
  * Serves as a model for all the types of completions available ([MarkedCompletedData] and
@@ -26,6 +31,77 @@ open class MarkedDataInt(
     val notes: String?,
     val likedBy: MutableList<String>
 ) {
+    companion object {
+        /**
+         * Initializes a new [MarkedDataInt] subinstance based on the data of [document].
+         * @author Arnau Mora
+         * @since 20210719
+         * @param document The document to get the data from.
+         * @return A [MarkedDataInt] class if everything went correctly. Null otherwise.
+         */
+        suspend fun newInstance(document: DocumentSnapshot): MarkedDataInt? {
+            val documentPath = document.reference.path
+            val timestamp = document.getTimestamp("timestamp")
+            val userUid = document.getString("user")!!
+            val attempts = document.getLong("attempts") ?: 0
+            val falls = document.getLong("falls") ?: 0
+            val comment = document.getString("comment")
+            val notes = document.getString("notes")
+            val grade = document.getString("grade") ?: ""
+            val project = document.getBoolean("project") ?: false
+            val typeRaw = document.getString("type")
+            // TODO: Load liked by
+
+            var type: CompletionType? = null
+            for (t in CompletionType.values())
+                if (t.id.equals(typeRaw, true))
+                    type = t
+
+            Timber.v("Got completion data for \"$documentPath\".")
+            val user = try {
+                Timber.v("Loading user data ($userUid) from server...")
+                val user = User(userUid)
+                user.getVisibleUserData()
+            } catch (e: IllegalArgumentException) {
+                Timber.w(e)
+                null
+            } catch (e: FirebaseFunctionsException) {
+                Timber.e(e, "Could not get VisibleUserData!")
+                null
+            }
+
+            if (user == null) {
+                Timber.i("Could not find user, continuing loop.")
+                return null
+            }
+
+            Timber.i("Processing path completed data result...")
+            return when {
+                project -> MarkedProjectData(
+                    document.reference.path,
+                    timestamp,
+                    user,
+                    comment,
+                    notes,
+                    listOf()
+                )
+                type != null -> MarkedCompletedData(
+                    document.reference.path,
+                    timestamp,
+                    user,
+                    attempts,
+                    falls,
+                    grade,
+                    type,
+                    comment,
+                    notes,
+                    listOf()
+                )
+                else -> null
+            }
+        }
+    }
+
     /**
      * Gets the amount of likes people have made on the publication.
      * @author Arnau Mora
