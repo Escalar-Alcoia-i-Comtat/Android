139cd2aaefe253611f22cad984974a295fffdb5b
Arnau Mora <arnyminer.z@gmail.com>
2021-07-21 22:30:01 +0200

    :sparkles: Added `DataClassViewModel`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/ui/viewmodel/DataClassViewModel.kt
@@ -0,0 +1,15 @@
+package com.arnyminerz.escalaralcoiaicomtat.instant.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+abstract class DataClassViewModel<T : DataClass<*, *>> : ViewModel() {
+    protected val firestore = Firebase.firestore
+    protected val storage = Firebase.storage
+
+    abstract val items: LiveData<List<T>>
+}
