676c1a6d705b3afc1e6329fb3c12847e341ced82
Arnau Mora <arnyminer.z@gmail.com>
2021-07-22 00:23:17 +0200

    :sparkles: Added `ZoomableImage`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/ui/elements/ZoomableImage.kt
@@ -0,0 +1,49 @@
+package com.arnyminerz.escalaralcoiaicomtat.instant.ui.elements
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.input.pointer.pointerInput
+
+@Composable
+fun ZoomableImage(painter: Painter) {
+    val scale = remember { mutableStateOf(1f) }
+    val rotationState = remember { mutableStateOf(1f) }
+    Box(
+        modifier = Modifier
+            .clip(RectangleShape) // Clip the box content
+            .fillMaxSize() // Give the size you want...
+            .background(Color.Gray)
+            .pointerInput(Unit) {
+                detectTransformGestures { _, _, zoom, rotation ->
+                    scale.value *= zoom
+                    rotationState.value += rotation
+                }
+            }
+    ) {
+        Image(
+            modifier = Modifier
+                .align(Alignment.Center) // keep the image centralized into the Box
+                .graphicsLayer(
+                    // adding some zoom limits (min 50%, max 200%)
+                    scaleX = maxOf(.5f, minOf(3f, scale.value)),
+                    scaleY = maxOf(.5f, minOf(3f, scale.value)),
+                    rotationZ = rotationState.value
+                ),
+            contentDescription = null,
+            painter = painter
+        )
+    }
+}
