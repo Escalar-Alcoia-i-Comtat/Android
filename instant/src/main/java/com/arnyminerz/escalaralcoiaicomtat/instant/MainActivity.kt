e15e9a24d92b0434f157a81869539931a5427641
Arnau Mora <arnyminer.z@gmail.com>
2021-07-21 22:30:57 +0200

    :sparkles: Started using `Explorer`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/MainActivity.kt
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/MainActivity.kt
@@ -2,35 +2,21 @@ package com.arnyminerz.escalaralcoiaicomtat.instant
 
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
 import androidx.annotation.UiThread
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -43,20 +29,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
-import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -64,12 +42,9 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navArgument
 import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_ERROR_REPORTING_PREF
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
+import com.arnyminerz.escalaralcoiaicomtat.instant.ui.climb.Explorer
 import com.arnyminerz.escalaralcoiaicomtat.instant.ui.theme.EscalarAlcoiaIComtatTheme
-import com.arnyminerz.escalaralcoiaicomtat.instant.ui.theme.ItemTextBackground
 import com.arnyminerz.escalaralcoiaicomtat.instant.ui.viewmodel.AreasViewModel
 import com.google.android.gms.instantapps.InstantApps
 import com.google.firebase.analytics.ktx.analytics
@@ -200,11 +175,11 @@ fun MainView(activity: Activity) {
                     shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
                 ) {
                     NavHost(navController = navController, startDestination = "root") {
-                        composable("root") { MainContent(activity, navController) }
+                        composable("root") { AreasExplorer(activity, navController) }
                         composable(
                             "explore_area",
                             arguments = listOf(navArgument("areaId") { type = NavType.StringType })
-                        ) { MainContent(activity, navController) }
+                        ) { ZonesExplorer(activity, navController) }
                     }
                 }
             }
@@ -216,112 +191,14 @@ fun MainView(activity: Activity) {
 @ExperimentalMaterialApi
 @ExperimentalAnimationApi
 @ExperimentalCoilApi
-fun MainContent(activity: Activity, navController: NavController) {
-    val areasViewModel = AreasViewModel()
-    val areasLiveData = areasViewModel.areas
-    val areas: List<Area> by areasLiveData.observeAsState(listOf())
-    var isLoading by remember { mutableStateOf(true) }
-
-    areasLiveData.observe(activity as LifecycleOwner) { isLoading = it.isEmpty() }
-
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 24.dp)
-    ) {
-        AnimatedVisibility(visible = isLoading, modifier = Modifier.size(52.dp)) {
-            CircularProgressIndicator()
-        }
-    }
-    AreasList(areas, R.drawable.ic_wide_placeholder, navController)
+fun AreasExplorer(activity: Activity, navController: NavController) {
+    Explorer(activity, navController, AreasViewModel::class.java)
 }
 
 @Composable
+@ExperimentalMaterialApi
+@ExperimentalAnimationApi
 @ExperimentalCoilApi
-fun AreasList(
-    areas: List<Area>,
-    @DrawableRes placeholder: Int,
-    navController: NavController
-) {
-    Timber.v("Loading areas list...")
-    val state = rememberLazyListState()
-    LazyColumn(
-        state = state,
-        modifier = Modifier.padding(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        // App contents
-        items(areas) { area ->
-            Timber.v("A/$area > Iterating...")
-            val downloadUrl = area.downloadUrl
-            if (downloadUrl == null)
-                Timber.i("A/$area > Could not load image since downloadUrl is null")
-            else
-                area.AreaItem(navController, placeholder, downloadUrl)
-        }
-    }
-}
-
-@Composable
-@ExperimentalCoilApi
-fun Area.AreaItem(navController: NavController, @DrawableRes placeholder: Int, image: Uri) {
-    val context = LocalContext.current
-    var boxSize by remember { mutableStateOf(Size.Zero) }
-    var height: Float by remember { mutableStateOf(0f) }
-
-    Timber.v("$this > Composing area $displayName...")
-    Timber.v("$this > Url: $image")
-    Card(
-        modifier = Modifier
-            .clickable {
-                toast(context, "Clicked $displayName")
-            }
-    ) {
-        Box {
-            Image(
-                painter = rememberImagePainter(
-                    data = image.toString(),
-                    onExecute = { _, current ->
-                        val size = current.size
-                        val scale = boxSize.height / boxSize.width
-                        height = size.height * scale
-                        true
-                    },
-                    builder = {
-                        placeholder(placeholder)
-                    }
-                ),
-                contentScale = ContentScale.Crop,
-                contentDescription = "Area image",
-                modifier = Modifier
-                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
-                    .height(150.dp)
-                    .fillMaxWidth()
-                    .onGloballyPositioned { layoutCoordinates ->
-                        boxSize = layoutCoordinates.size.toSize()
-                    }
-            )
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(height.dp),
-                verticalAlignment = Alignment.Bottom
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(ItemTextBackground)
-                        .clip(RoundedCornerShape(16.dp))
-                ) {
-                    Text(
-                        text = displayName,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth()
-                    )
-                }
-            }
-        }
-    }
+fun ZonesExplorer(activity: Activity, navController: NavController) {
+    Text(text = "This is a Zone!")
 }
