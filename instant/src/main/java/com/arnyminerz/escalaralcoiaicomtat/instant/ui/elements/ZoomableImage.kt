f417fbc9c5792dd08e4b14d0314c888469f94f1f
Arnau Mora <arnyminer.z@gmail.com>
2021-07-22 01:39:29 +0200

    :sparkles: Added `modifier`, `enableScale`, `enableRotation`, `minZoom`, `maxZoom`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/ui/elements/ZoomableImage.kt
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/instant/ui/elements/ZoomableImage.kt
@@ -1,36 +1,42 @@
 package com.arnyminerz.escalaralcoiaicomtat.instant.ui.elements
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.input.pointer.pointerInput
 
 @Composable
-fun ZoomableImage(painter: Painter) {
+fun ZoomableImage(
+    painter: Painter,
+    modifier: Modifier = Modifier,
+    enableScale: Boolean = true,
+    enableRotation: Boolean = true,
+    minZoom: Float = .5f,
+    maxZoom: Float = 2f
+) {
     val scale = remember { mutableStateOf(1f) }
-    val rotationState = remember { mutableStateOf(1f) }
+    val rotationState = remember { mutableStateOf(0f) }
     Box(
-        modifier = Modifier
-            .clip(RectangleShape) // Clip the box content
-            .fillMaxSize() // Give the size you want...
-            .background(Color.Gray)
+        modifier = modifier
             .pointerInput(Unit) {
                 detectTransformGestures { _, _, zoom, rotation ->
-                    scale.value *= zoom
-                    rotationState.value += rotation
+                    if (enableScale)
+                        scale.value *= zoom
+                    if (enableRotation)
+                        rotationState.value += rotation
                 }
+                detectTapGestures(onDoubleTap = {
+                    if (enableScale)
+                        scale.value *= 2
+                })
             }
     ) {
         Image(
@@ -38,8 +44,8 @@ fun ZoomableImage(painter: Painter) {
                 .align(Alignment.Center) // keep the image centralized into the Box
                 .graphicsLayer(
                     // adding some zoom limits (min 50%, max 200%)
-                    scaleX = maxOf(.5f, minOf(3f, scale.value)),
-                    scaleY = maxOf(.5f, minOf(3f, scale.value)),
+                    scaleX = maxOf(minZoom, minOf(maxZoom, scale.value)),
+                    scaleY = maxOf(minZoom, minOf(maxZoom, scale.value)),
                     rotationZ = rotationState.value
                 ),
             contentDescription = null,
