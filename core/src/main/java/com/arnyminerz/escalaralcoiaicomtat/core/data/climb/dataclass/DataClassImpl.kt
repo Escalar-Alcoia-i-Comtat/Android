205502947e6a65edbce2bb4d6b186cc17534e514
Arnau Mora <arnyminer.z@gmail.com>
2021-07-22 16:01:26 +0200

    :fire: Removed `Document` annotation
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/dataclass/DataClassImpl.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/dataclass/DataClassImpl.kt
@@ -1,16 +1,14 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass
 
 import android.os.Parcelable
-import androidx.appsearch.annotation.Document
 import kotlinx.parcelize.Parcelize
 import java.util.Date
 
-@Document
 @Parcelize
 open class DataClassImpl(
-    @Document.Id open val objectId: String,
-    @Document.Namespace val namespace: String,
-    @Document.CreationTimestampMillis val timestampMillis: Long
+    open val objectId: String,
+    val namespace: String,
+    val timestampMillis: Long
 ) : Parcelable {
     companion object {
         fun find(list: List<DataClassImpl>, objectId: String): Int {
