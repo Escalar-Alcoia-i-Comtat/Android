3cd495ec582c161464483524ac3b78fe5a80a807
Arnau Mora <arnyminer.z@gmail.com>
2021-07-23 11:49:41 +0200

    :truck: Moving main app to Compose
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.arnyminerz.escalaralcoiaicomtat.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Lime700 = Color(0xFFAFB42B)
+val Lime800 = Color(0xFF9E9D24)
+val Lime900 = Color(0xFF827717)
+val Lime500 = Color(0xFFCEDC39)
+
+val Green400 = Color(0xff66bb6a)
+
+val Purple300 = Color(0xffbb66b7)
+
+val Yellow100 = Color(0xffFFF9C4)
+
+val Amber50 = Color(0xffFFF8E1)
+
+val ItemTextBackground = Color(0x4B000000)
