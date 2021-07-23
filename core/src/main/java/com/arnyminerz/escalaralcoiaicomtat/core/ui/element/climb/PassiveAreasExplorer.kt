92e60a7ebab478c1bbf75a087820786fe8a108f6
Arnau Mora <arnyminer.z@gmail.com>
2021-07-23 16:45:01 +0200

    :sparkles: Added `PassiveAreasExplorer`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/element/climb/PassiveAreasExplorer.kt
@@ -0,0 +1,19 @@
+package com.arnyminerz.escalaralcoiaicomtat.core.ui.element.climb
+
+import android.app.Activity
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import coil.annotation.ExperimentalCoilApi
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel.PassiveAreasViewModel
+
+@Composable
+@ExperimentalMaterialApi
+@ExperimentalAnimationApi
+@ExperimentalCoilApi
+@ExperimentalFoundationApi
+fun PassiveAreasExplorer(activity: Activity, navController: NavController) {
+    Explorer(activity, navController, dataClassViewModel = PassiveAreasViewModel::class.java)
+}
