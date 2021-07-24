2c4e6ee15e24ee825b3c5450fa77636d7e61040c
Arnau Mora <arnyminer.z@gmail.com>
2021-07-24 12:28:10 +0200

    :sparkles: Added `application`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/PassiveAreasViewModel.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/PassiveAreasViewModel.kt
@@ -1,11 +1,12 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.liveData
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.AREAS
 
-class PassiveAreasViewModel : DataClassViewModel<Area>() {
+class PassiveAreasViewModel(application: Application) : DataClassViewModel<Area>(application) {
     override val columnsPerRow: Int = 1
 
     override val items: LiveData<List<Area>> = liveData { emit(AREAS) }
