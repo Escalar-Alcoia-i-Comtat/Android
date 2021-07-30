6d81f90f23745bc99dbde680f84bb7a061874346
Arnau Mora <arnyminer.z@gmail.com>
2021-07-30 17:58:47 +0200

    :sparkles: Added `LoadingIndicator`

---
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/elements/LoadingIndicator.kt
@@ -0,0 +1,38 @@
+package com.arnyminerz.escalaralcoiaicomtat.ui.elements
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * An indicator to show the user when the app is loading content. Automatically shows and hides
+ * depending on [isLoading] with an animation.
+ * @author Arnau Mora
+ * @since 20210730
+ * @param isLoading Whether or not the content is being loaded, this is, whether or not to show the
+ * indicator.
+ * @param size The size of the indicator.
+ */
+@ExperimentalAnimationApi
+@Composable
+fun LoadingIndicator(isLoading: Boolean, size: Dp = 52.dp) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 24.dp)
+    ) {
+        AnimatedVisibility(visible = isLoading, modifier = Modifier.size(size)) {
+            CircularProgressIndicator()
+        }
+    }
+}
