e8c64a8f40dcd7d43ef1cb6516d91205ca160d3d
Arnau Mora <arnyminer.z@gmail.com>
2021-07-30 18:13:52 +0200

    :broom: Cleaned up code

--- instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/MainActivity.kt
+++ instant/src/main/java/com/arnyminerz/escalaralcoiaicomtat/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.arnyminerz.escalaralcoiaicomtat
 
 import android.app.Activity
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.ExperimentalFoundationApi
@@ -21,8 +19,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FloatingActionButton
@@ -56,7 +52,6 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -67,17 +62,19 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.AFTERNOON
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.ALL_DAY
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.MORNING
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.NO_SUN
-import com.arnyminerz.escalaralcoiaicomtat.core.ui.animation.EnterAnimation
-import com.arnyminerz.escalaralcoiaicomtat.core.ui.element.climb.AreasExplorer
-import com.arnyminerz.escalaralcoiaicomtat.core.ui.element.climb.SectorsExplorer
-import com.arnyminerz.escalaralcoiaicomtat.core.ui.element.climb.ZonesExplorer
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel.SectorViewModel
 import com.arnyminerz.escalaralcoiaicomtat.shared.APP_TYPE_PROP
 import com.arnyminerz.escalaralcoiaicomtat.shared.STATUS_INSTALLED
 import com.arnyminerz.escalaralcoiaicomtat.shared.STATUS_INSTANT
 import com.arnyminerz.escalaralcoiaicomtat.ui.elements.Chip
+import com.arnyminerz.escalaralcoiaicomtat.ui.elements.ExpandableHeader
+import com.arnyminerz.escalaralcoiaicomtat.ui.elements.InstallButton
 import com.arnyminerz.escalaralcoiaicomtat.ui.elements.LoadingIndicator
 import com.arnyminerz.escalaralcoiaicomtat.ui.elements.ZoomableImage
+import com.arnyminerz.escalaralcoiaicomtat.ui.navigation.areas
+import com.arnyminerz.escalaralcoiaicomtat.ui.navigation.sector
+import com.arnyminerz.escalaralcoiaicomtat.ui.navigation.sectors
+import com.arnyminerz.escalaralcoiaicomtat.ui.navigation.zones
 import com.arnyminerz.escalaralcoiaicomtat.ui.theme.EscalarAlcoiaIComtatTheme
 import com.google.android.gms.instantapps.InstantApps
 import com.google.firebase.analytics.ktx.analytics
@@ -152,87 +149,24 @@ fun MainView(activity: Activity, path: String? = null) {
         },
         backgroundColor = MaterialTheme.colors.primary,
         content = {
-            Column {
-                // Top Menu
-                AnimatedVisibility(visible = expanded) {
-                    Column(
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Button(
-                            onClick = {
-                                val postInstall = Intent(Intent.ACTION_MAIN)
-                                    .addCategory(Intent.CATEGORY_DEFAULT)
-                                    .setPackage("com.arnyminerz.escalaralcoiaicomtat")
-                                InstantApps.showInstallPrompt(activity, postInstall, 0, null)
-                            },
-                            modifier = Modifier.align(Alignment.End),
-                            colors = ButtonDefaults.textButtonColors(
-                                backgroundColor = MaterialTheme.colors.secondary,
-                                contentColor = MaterialTheme.colors.onSecondary
-                            ),
-                        ) {
-                            Text("Install App")
-                        }
-                    }
-                }
-
-                // Content card
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .animateContentSize(),
-                    shape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp)
+            ExpandableHeader(expanded, headerContent = {
+                InstallButton(activity)
+            }) {
+                NavHost(
+                    navController = navController,
+                    startDestination = "Areas"
                 ) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = "Areas"
-                    ) {
-                        composable("Areas") {
-                            EnterAnimation {
-                                AreasExplorer(activity, navController)
-                            }
-                        }
-                        composable("Areas/{areaId}") { backStackEntry ->
-                            val areaId = backStackEntry.arguments?.getString("areaId")
-                            if (areaId != null)
-                                EnterAnimation {
-                                    ZonesExplorer(activity, navController, areaId)
-                                }
-                            else
-                                Text(text = "Could not navigate to area: $areaId")
-                        }
-                        composable("Areas/{areaId}/Zones/{zoneId}") { backStackEntry ->
-                            val areaId = backStackEntry.arguments?.getString("areaId")
-                            val zoneId = backStackEntry.arguments?.getString("zoneId")
-                            if (areaId != null && zoneId != null)
-                                EnterAnimation {
-                                    SectorsExplorer(activity, navController, areaId, zoneId)
-                                }
-                            else
-                                Text(text = "Could not navigate to zone Z/$zoneId in A/$areaId")
-                        }
-                        composable("Areas/{areaId}/Zones/{zoneId}/Sectors/{sectorId}") { backStackEntry ->
-                            val areaId = backStackEntry.arguments?.getString("areaId")
-                            val zoneId = backStackEntry.arguments?.getString("zoneId")
-                            val sectorId = backStackEntry.arguments?.getString("sectorId")
-                            if (areaId != null && zoneId != null && sectorId != null)
-                                EnterAnimation {
-                                    SectorView(activity, areaId, zoneId, sectorId)
-                                }
-                            else
-                                Text(text = "Could not navigate to sector S/$sectorId in Z/$zoneId in A/$areaId")
-                        }
+                    areas(activity, navController)
+                    zones(activity, navController)
+                    sectors(activity, navController)
+                    sector(activity)
 
-                        if (path != null && path.isNotEmpty())
-                            try {
-                                navController.navigate(path)
-                            } catch (e: IllegalArgumentException) {
-                                Timber.e(e, "Could not navigate to $path")
-                            }
-                    }
+                    if (path != null && path.isNotEmpty())
+                        try {
+                            navController.navigate(path)
+                        } catch (e: IllegalArgumentException) {
+                            Timber.e(e, "Could not navigate to $path")
+                        }
                 }
             }
         },
