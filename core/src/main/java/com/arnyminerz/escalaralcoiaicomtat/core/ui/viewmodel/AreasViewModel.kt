e0da4ceabbdab1966ee5a993392ea7be2a2ce4b1
Arnau Mora <arnyminer.z@gmail.com>
2021-08-10 11:40:49 +0200

    :sparkles: Added `progress` live data
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/AreasViewModel.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/AreasViewModel.kt
@@ -3,21 +3,27 @@ package com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel
 import android.app.Activity
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.loadAreas
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.AREAS
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.ValueMax
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.asyncCoroutineScope
 import timber.log.Timber
 
 class AreasViewModel<A : Activity>(activity: A) : DataClassViewModel<Area, A>(activity) {
     override val columnsPerRow: Int = 1
 
+    val progress: MutableLiveData<ValueMax<Int>> by lazy { MutableLiveData(ValueMax(0, 0)) }
+
     override val items: LiveData<List<Area>> = liveData(asyncCoroutineScope.coroutineContext) {
         if (AREAS.isEmpty()) {
+            Timber.v("Areas is empty, loading...")
             val application = (context as? Activity)?.application ?: context as Application
             firestore.loadAreas(application, progressCallback = { current, total ->
                 Timber.i("Loading areas: $current/$total")
+                progress.value = ValueMax(current, total)
             })
         }
         emit(AREAS)
