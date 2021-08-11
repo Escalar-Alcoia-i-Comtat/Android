6c58049adcf0c1d098b810c1da9049470ddf13f1
Arnau Mora <arnyminer.z@gmail.com>
2021-08-11 13:08:49 +0200

    :sparkles: Added `SearchItemTypeColor`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/Colors.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/Colors.kt
@@ -4,3 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val ItemTextBackground = Color(0x4B000000)
 val ItemTextColor = Color(0xffeeeeee)
+
+val SearchItemTypeColor = Color(0xff303F9F) // Indigo 700
