d2a1f8b76d3cf5e3a4444d94511d683828ca3648
Arnau Mora <arnyminer.z@gmail.com>
2021-08-11 13:09:09 +0200

    :sparkles: Added `cabinFamily`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/Fonts.kt
@@ -0,0 +1,7 @@
+package com.arnyminerz.escalaralcoiaicomtat.core.ui
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.arnyminerz.escalaralcoiaicomtat.core.R
+
+val cabinFamily = FontFamily(Font(R.font.cabin))
