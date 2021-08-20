7b8a38109ac642eb7f3c069acc10ab05e48f7a39
Arnau Mora <arnyminer.z@gmail.com>
2021-08-20 11:11:00 +0200

    :sparkles: Added `DataClassListViewModelFactory`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

---
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/view/model/DataClassListViewModelFactory.kt
@@ -0,0 +1,19 @@
+package com.arnyminerz.escalaralcoiaicomtat.view.model
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.App
+
+class DataClassListViewModelFactory(
+    private val app: App,
+    private val areaId: String?,
+    private val zoneId: String?,
+    private val sectorId: String?
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DataClassListViewModel::class.java))
+            return DataClassListViewModel(app, areaId, zoneId, sectorId) as T
+
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
