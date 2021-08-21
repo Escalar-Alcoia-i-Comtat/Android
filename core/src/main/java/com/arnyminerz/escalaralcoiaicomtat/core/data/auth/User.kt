dd5f9c1d338f3675ed61573b42ed690af45c1012
Arnau Mora <arnyminer.z@gmail.com>
2021-08-21 20:59:41 +0200

    :sparkles: Added `getCompletedPaths`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/auth/User.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/auth/User.kt
@@ -1,10 +1,13 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.data.auth
 
 import androidx.annotation.WorkerThread
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.completion.storage.MarkedDataInt
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.functions.FirebaseFunctionsException
 import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -37,5 +40,27 @@ class User(val uid: String) {
                 val visibleUserData = VisibleUserData(uid, displayName, profileImagePath)
                 cont.resume(visibleUserData)
             }
+    }
+
+    /**
+     * Fetches all the paths the user has marked as complete.
+     * @author Arnau Mora
+     * @since 20210821
+     */
+    @WorkerThread
+    suspend fun getCompletedPaths(): List<MarkedDataInt> {
+        val firestore = Firebase.firestore
+        val completionsCollection = firestore
+            .collectionGroup("Completions")
+            .orderBy("timestamp")
+            .get()
+            .await()
+        val completionsDocuments = completionsCollection.documents.ifEmpty { return emptyList() }
+        return arrayListOf<MarkedDataInt>().apply {
+            for (document in completionsDocuments) {
+                val markedDataInt = MarkedDataInt.newInstance(document) ?: continue
+                add(markedDataInt)
+            }
+        }
     }
 }
