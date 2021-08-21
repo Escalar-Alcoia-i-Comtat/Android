de98e4ceea7a16d85117708c923accbfd7c40775
Arnau Mora <arnyminer.z@gmail.com>
2021-08-21 20:17:20 +0200

    :sparkles: Added `newInstance`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/activity/LastActivityFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/activity/LastActivityFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.UserActivity
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.ARGUMENT_ACTIVITY
 import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentLastActivityBinding
 
 /**
@@ -12,7 +14,22 @@ import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentLastActivityBindi
  * @author Arnau Mora
  * @since 20210821
  */
-class LastActivityFragment : Fragment() {
+class LastActivityFragment private constructor() : Fragment() {
+    companion object {
+        /**
+         * Creates a new instance of the [LastActivityFragment] with the specified arguments.
+         * @author Arnau Mora
+         * @since 20210821
+         * @param userActivity The last activity the user has performed.
+         */
+        fun newInstance(userActivity: UserActivity): LastActivityFragment =
+            LastActivityFragment().apply {
+                val args = arguments ?: return@apply
+                args.putParcelable(ARGUMENT_ACTIVITY, userActivity)
+                arguments = args
+            }
+    }
+
     /**
      * Stores the View Binding for updating the UI.
      * @author Arnau Mora
