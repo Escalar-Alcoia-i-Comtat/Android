111f00030741a7d00b9926d72dd4cc8a4b8a8ec4
Arnau Mora <arnyminer.z@gmail.com>
2021-08-21 12:14:18 +0200

    :sparkles: Using Activity intents
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/DataClassExtension.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/DataClassExtension.kt
@@ -1,22 +1,15 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity
 
 import android.app.Activity
-import android.content.Intent
 import androidx.annotation.WorkerThread
 import com.arnyminerz.escalaralcoiaicomtat.activity.climb.AreaActivity
 import com.arnyminerz.escalaralcoiaicomtat.activity.climb.SectorActivity
 import com.arnyminerz.escalaralcoiaicomtat.activity.climb.ZoneActivity
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClassImpl
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.get
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.Path
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_AREA
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_SECTOR
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_ZONE
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.app
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.putExtra
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.uiContext
 import timber.log.Timber
 
@@ -29,37 +22,20 @@ import timber.log.Timber
 @WorkerThread
 @Throws(IllegalArgumentException::class)
 suspend fun DataClassImpl.launch(activity: Activity) {
-    val activityClass: Class<*> = when (namespace) {
-        Area.NAMESPACE -> AreaActivity::class.java
-        Zone.NAMESPACE -> ZoneActivity::class.java
-        Sector.NAMESPACE -> SectorActivity::class.java
-        Path.NAMESPACE -> SectorActivity::class.java
-        else -> throw IllegalArgumentException("Cannot launch activity since $namespace is not a valid namespace.")
-    }
-    val intent = Intent(activity, activityClass)
     val pathPieces = documentPath.split("/")
     Timber.v("Launching activity with path $documentPath")
-    when (namespace) {
-        Area.NAMESPACE -> {
-            intent.putExtra(EXTRA_AREA, pathPieces[1]) // area ID
-        }
-        Zone.NAMESPACE -> {
-            intent.putExtra(EXTRA_AREA, pathPieces[1]) // area ID
-            intent.putExtra(EXTRA_ZONE, pathPieces[3]) // zone ID
-        }
-        Sector.NAMESPACE, Path.NAMESPACE -> {
-            intent.putExtra(EXTRA_AREA, pathPieces[1]) // area ID
-            intent.putExtra(EXTRA_ZONE, pathPieces[3]) // zone ID
-            Timber.v("Getting sectors for zone ${pathPieces[3]}...")
-            val app = activity.app
-            val areas = app.getAreas()
-            val sectors = areas[pathPieces[1]]
-                ?.get(app.searchSession, pathPieces[3])
-                ?.getChildren(app.searchSession)
-            if (sectors == null)
-                Timber.e("Could not load sectors from area ${pathPieces[1]}, sector ${pathPieces[3]}")
-            intent.putExtra(EXTRA_SECTOR, pathPieces[5])
-        }
+    // 1 -> Area ID
+    // 3 -> Zone ID
+    // 5 -> Sector ID
+    val intent = when (namespace) {
+        Area.NAMESPACE ->
+            AreaActivity.intent(activity, pathPieces[1]) // area ID
+        Zone.NAMESPACE ->
+            ZoneActivity.intent(activity, pathPieces[3]) // zone ID
+        Sector.NAMESPACE, Path.NAMESPACE ->
+            SectorActivity.intent(activity, pathPieces[3], pathPieces[5])
+        else ->
+            throw IllegalArgumentException("Cannot launch activity since $namespace is not a valid namespace.")
     }
     uiContext { activity.startActivity(intent) }
 }
