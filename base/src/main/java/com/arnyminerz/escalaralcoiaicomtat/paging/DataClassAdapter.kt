276115ba8b74975a4aea4d2bf4ebaacf74dfbf28
Arnau Mora <arnyminer.z@gmail.com>
2021-08-22 11:52:10 +0200

    :sparkles: Added `DATACLASS_PREVIEW_SCALE`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/paging/DataClassAdapter.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/paging/DataClassAdapter.kt
@@ -15,6 +15,7 @@ import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClassImpl
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DownloadStatus
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.Path
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.DATACLASS_PREVIEW_SCALE
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.app
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.then
@@ -174,7 +175,7 @@ open class DataClassAdapter(
                     val image = dataClass.image(
                         context,
                         storage,
-                        ImageLoadParameters().withResultImageScale(.5f)
+                        ImageLoadParameters().withResultImageScale(DATACLASS_PREVIEW_SCALE)
                     ) { }
                     uiContext {
                         if (image != null)
