9ce12d5bf5fd91a936f838d794e4d990712aff8e
Arnau Mora <arnyminer.z@gmail.com>
2021-08-22 11:39:11 +0200

    :sparkles: Added completions loading
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/activity/LastActivityFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/activity/LastActivityFragment.kt
@@ -8,9 +8,17 @@ import androidx.annotation.StringRes
 import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
 import com.arnyminerz.escalaralcoiaicomtat.R
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.UserActivity
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.ARGUMENT_ACTIVITY
+import com.arnyminerz.escalaralcoiaicomtat.activity.profile.UserActivityActivity
+import com.arnyminerz.escalaralcoiaicomtat.core.data.auth.User
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.completion.storage.MarkedDataInt
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.completion.storage.MarkedProjectData
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.ARGUMENT_USER_ID
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.uiContext
+import com.arnyminerz.escalaralcoiaicomtat.core.view.hide
+import com.arnyminerz.escalaralcoiaicomtat.core.view.show
+import com.arnyminerz.escalaralcoiaicomtat.core.view.visibility
 import com.arnyminerz.escalaralcoiaicomtat.databinding.FragmentLastActivityBinding
 import timber.log.Timber
 
@@ -25,12 +33,12 @@ class LastActivityFragment private constructor() : Fragment() {
          * Creates a new instance of the [LastActivityFragment] with the specified arguments.
          * @author Arnau Mora
          * @since 20210821
-         * @param userActivity The last activity the user has performed.
+         * @param userId The id of the user to fetch the last activity from.
          */
-        fun newInstance(userActivity: UserActivity): LastActivityFragment =
+        fun newInstance(userId: String): LastActivityFragment =
             LastActivityFragment().apply {
-                val args = arguments ?: return@apply
-                args.putParcelable(ARGUMENT_ACTIVITY, userActivity)
+                val args = arguments ?: Bundle()
+                args.putString(ARGUMENT_USER_ID, userId)
                 arguments = args
             }
     }
@@ -43,11 +51,11 @@ class LastActivityFragment private constructor() : Fragment() {
     private var binding: FragmentLastActivityBinding? = null
 
     /**
-     * Stores the [UserActivity] that should be displayed to the user.
+     * Stores the id of the user to display the last activity from.
      * @author Arnau Mora
      * @since 20210821
      */
-    private lateinit var userActivity: UserActivity
+    private lateinit var userId: String
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -85,14 +93,98 @@ class LastActivityFragment private constructor() : Fragment() {
 
         val args =
             arguments ?: return initException("Arguments is null", R.string.toast_error_internal)
-        val activity = args.getParcelable<UserActivity>(ARGUMENT_ACTIVITY)
+        userId = args.getString(ARGUMENT_USER_ID)
             ?: return initException(
-                "Activity not found in arguments",
+                "User ID not found in arguments",
                 R.string.toast_error_internal
             )
-        userActivity = activity
 
         Timber.i("Loaded user activity successfully.")
+    }
+
+    /**
+     * Updates the UI according to [userId].
+     * @author Arnau Mora
+     * @since 20210821
+     */
+    @UiThread
+    override fun onStart() {
+        super.onStart()
+
+        if (binding == null)
+            return Timber.e("Binding is null, won't update UI.")
+        if (!this::userId.isInitialized)
+            return Timber.e("userId not initialized.")
+
+        binding!!.lastActivityLayout.hide()
+        binding!!.noActivitiesTextView.hide()
+        binding!!.lastActivityProgressIndicator.visibility(true)
+
+        // Load the last activity
+        doAsync {
+            Timber.v("Getting data of user $userId...")
+            val user = User(userId)
+            val completedPaths = user.getCompletedPaths()
+            Timber.v("Got ${completedPaths.size} completed paths.")
+
+            if (completedPaths.isEmpty())
+                return@doAsync uiContext {
+                    if (binding == null)
+                        Timber.e("Binding is null, won't update UI.")
+                    else {
+                        Timber.v("Showing no activities text view.")
+                        binding!!.lastActivityProgressIndicator.visibility(false)
+                        binding!!.noActivitiesTextView.show()
+                    }
+                }
+
+            val completedPath = completedPaths[0]
+            Timber.v("Getting completed path...")
+            val zone = completedPath.getZone()
+            var completions = 0
+            var projects = 0
+            val activityCompletions = arrayListOf<MarkedDataInt>()
+                .apply {
+                    for (completion in completedPaths) {
+                        val zoneReference = completion.zoneReference()
+                        val zonePath = zoneReference.path
+                        if (// Zone is the desired one
+                            zone.documentPath == zonePath &&
+                            // And path is not the selected one
+                            completion.documentPath != completedPath.documentPath
+                        ) {
+                            add(completion)
+                            if (completion is MarkedProjectData)
+                                projects++
+                            else
+                                completions++
+                        }
+                    }
+                }
+            Timber.v("Processing completions that match zone...")
+            uiContext {
+                if (binding == null)
+                    Timber.e("Binding is null, won't update UI.")
+                else {
+                    binding!!.lastActivityPlaceTextView.text = zone.displayName
+                    binding!!.lastActivitySummaryTextView.text = resources.getString(
+                        R.string.profile_last_activity_summary,
+                        completions, projects
+                    )
+                    binding!!.viewActivityButton.setOnClickListener {
+                        UserActivityActivity.intent(
+                            requireContext(),
+                            activityCompletions,
+                            completions,
+                            projects
+                        )
+                    }
+
+                    binding!!.lastActivityProgressIndicator.visibility(false)
+                    binding!!.lastActivityLayout.show()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
