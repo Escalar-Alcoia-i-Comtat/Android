57e7704321b4149e169904e39f56767d860ffa0c
Arnau Mora <arnyminer.z@gmail.com>
2021-08-28 16:30:32 +0200

    :fire: Removed `SETTINGS_PREVIEW_SCALE_PREF`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/GeneralSettingsFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/GeneralSettingsFragment.kt
@@ -8,17 +8,14 @@ import androidx.core.content.ContextCompat
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreference
 import com.arnyminerz.escalaralcoiaicomtat.R
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.LOCATION_PERMISSION_REQUEST_CODE
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREF_DISABLE_NEARBY
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREVIEW_SCALE_PREFERENCE_MULTIPLIER
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_CENTER_MARKER_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_ERROR_REPORTING_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_LANGUAGE_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_NEARBY_DISTANCE_PREF
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_PREVIEW_SCALE_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
 import timber.log.Timber
 
@@ -26,7 +23,6 @@ private const val PREVIEW_SCALE_REDUCER = 10f
 
 class GeneralSettingsFragment : PreferenceFragmentCompat() {
     private var errorReportingPreference: SwitchPreference? = null
-    private var previewScalePreference: SeekBarPreference? = null
     private var languagePreference: ListPreference? = null
     private var enableNearby: SwitchPreference? = null
     private var centerMarkerPreference: SwitchPreference? = null
@@ -111,14 +107,6 @@ class GeneralSettingsFragment : PreferenceFragmentCompat() {
             true
         }
 
-        previewScalePreference = findPreference("pref_preview_scale")
-        previewScalePreference?.setOnPreferenceChangeListener { _, value ->
-            SETTINGS_PREVIEW_SCALE_PREF.put(
-                (value as Int).toFloat() / PREVIEW_SCALE_REDUCER
-            )
-            true
-        }
-
         updateFields()
     }
 
@@ -147,9 +135,6 @@ class GeneralSettingsFragment : PreferenceFragmentCompat() {
         val nearbyDistancePref = SETTINGS_NEARBY_DISTANCE_PREF.get().toString()
         Timber.d("Nearby distance: $nearbyDistancePref")
         nearbyDistance?.text = nearbyDistancePref
-
-        previewScalePreference?.value =
-            (SETTINGS_PREVIEW_SCALE_PREF.get() * PREVIEW_SCALE_PREFERENCE_MULTIPLIER).toInt()
 
         errorReportingPreference?.isChecked = SETTINGS_ERROR_REPORTING_PREF.get()
     }
