7b3e273fa0e0db556e252254e84de7969dcabdbe
Arnau Mora <arnyminer.z@gmail.com>
2021-08-28 16:46:37 +0200

    :fire: Removed download quality preference
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/DownloadsSettingsFragment.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/fragment/preferences/DownloadsSettingsFragment.kt
@@ -2,12 +2,8 @@ package com.arnyminerz.escalaralcoiaicomtat.fragment.preferences
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
 import androidx.preference.SwitchPreference
 import com.arnyminerz.escalaralcoiaicomtat.R
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOADS_QUALITY_PREF
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_QUALITY_MAX
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_QUALITY_MIN
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_MOBILE_DOWNLOAD_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_ROAMING_DOWNLOAD_PREF
 
@@ -15,8 +11,6 @@ class DownloadsSettingsFragment : PreferenceFragmentCompat() {
 
     private var mobileDataDownloadPref: SwitchPreference? = null
     private var roamingDownloadPref: SwitchPreference? = null
-
-    private var downloadQualityPref: SeekBarPreference? = null
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_downloads, rootKey)
@@ -38,17 +32,6 @@ class DownloadsSettingsFragment : PreferenceFragmentCompat() {
 
             true
         }
-
-        // Todo: Remove preference
-        downloadQualityPref = findPreference("pref_download_quality")
-        downloadQualityPref?.value = DOWNLOADS_QUALITY_PREF.get()
-        downloadQualityPref?.max = DOWNLOAD_QUALITY_MAX
-        downloadQualityPref?.min = DOWNLOAD_QUALITY_MIN
-        downloadQualityPref?.setOnPreferenceChangeListener { _, value ->
-            DOWNLOADS_QUALITY_PREF.put(value as Int)
-
-            true
-        }
     }
 
     override fun onResume() {
@@ -59,10 +42,5 @@ class DownloadsSettingsFragment : PreferenceFragmentCompat() {
 
         roamingDownloadPref = roamingDownloadPref ?: findPreference("pref_roaming_download")
         roamingDownloadPref?.isChecked = SETTINGS_ROAMING_DOWNLOAD_PREF.get()
-
-        downloadQualityPref = downloadQualityPref ?: findPreference("pref_download_quality")
-        downloadQualityPref?.value = DOWNLOADS_QUALITY_PREF.get()
-        downloadQualityPref?.max = DOWNLOAD_QUALITY_MAX
-        downloadQualityPref?.min = DOWNLOAD_QUALITY_MIN
     }
 }
