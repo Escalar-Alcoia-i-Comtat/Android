86fc3799d11f4b183374665782efec8b5655f638
Arnau Mora <arnyminer.z@gmail.com>
2021-08-30 11:38:43 +0200

    :fire: Removed `UIMetadata`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/dataclass/UIMetadata.kt
+++
@@ -1,8 +0,0 @@
-package com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass
-
-import androidx.annotation.DrawableRes
-
-data class UIMetadata(
-    @DrawableRes val placeholderDrawable: Int,
-    @DrawableRes val errorPlaceholderDrawable: Int,
-)
