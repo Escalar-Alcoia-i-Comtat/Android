eb54a94bb5efb11533b7c96fd05c7d54c86d2310
Arnau Mora <arnyminer.z@gmail.com>
2021-08-30 11:27:38 +0200

    :sparkles: Added `showLocation`
    Signed-off-by: Arnau Mora <Arnyminer.Z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/dataclass/DataClassDisplayOptions.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/dataclass/DataClassDisplayOptions.kt
@@ -6,5 +6,26 @@ package com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass
  * @since 20210819
  * @param columns The amount of columns that should be shown to the user.
  * @param downloadable Whether or not the [DataClass] is downloadable.
+ * @param showLocation Whether or not the location button should be visible.
  */
-data class DataClassDisplayOptions(val columns: Int, val downloadable: Boolean)
+data class DataClassDisplayOptions(
+    /**
+     * The amount of columns that should be shown to the user.
+     * @author Arnau Mora
+     * @since 20210819
+     */
+    val columns: Int,
+    /**
+     * Whether or not the [DataClass] is downloadable.
+     * This determines if the download button should be shown.
+     * @author Arnau Mora
+     * @since 20210819
+     */
+    val downloadable: Boolean,
+    /**
+     * Whether or not the location button should be visible.
+     * @author Arnau Mora
+     * @since 20210830
+     */
+    val showLocation: Boolean
+)
