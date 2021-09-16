84cca2ecc76b04283f675215cb5eb00363a225aa
Arnau Mora <arnyminer.z@gmail.com>
2021-09-16 22:04:28 +0200

    :sparkles: Using binding

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/list/holder/PathEquipmentViewHolder.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/list/holder/PathEquipmentViewHolder.kt
@@ -1,16 +1,6 @@
 package com.arnyminerz.escalaralcoiaicomtat.list.holder
 
-import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
-import com.arnyminerz.escalaralcoiaicomtat.R
+import com.arnyminerz.escalaralcoiaicomtat.databinding.ListItemFixedEquipmentBinding
 
-class PathEquipmentViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val layout: LinearLayout = view.findViewById(R.id.fixedEquipment_layout)
-    val imageView: ImageView = view.findViewById(R.id.fixedEquipment_imageView)
-    val textView: TextView = view.findViewById(R.id.fixedEquipment_textView)
-    val cardView: CardView = view.findViewById(R.id.fixedEquipment_cardView)
-}
+class PathEquipmentViewHolder(val binding: ListItemFixedEquipmentBinding) : RecyclerView.ViewHolder(binding.root)
