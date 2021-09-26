4d95ba6672de59c8396a2f53b9fc41556a840941
Arnau Mora <arnyminer.z@gmail.com>
2021-09-26 14:07:34 +0200

    :sparkles: Added image files referencing

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/worker/UpdateWorker.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/worker/UpdateWorker.kt
@@ -15,11 +15,16 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREF_INDEXED_SEARCH
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_MOBILE_DOWNLOAD_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.SETTINGS_ROAMING_DOWNLOAD_PREF
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.ValueMax
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.deleteIfExists
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.storage.dataDir
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
 import timber.log.Timber
 import java.io.File
 import kotlin.random.Random
@@ -125,10 +130,24 @@ private constructor(appContext: Context, workerParams: WorkerParameters) :
         }
         updateProgress(PROGRESS_STEP_IMAGE_PROCESS, 100)
 
-        // Now, for each image, compare its metadata with the one from the server, and create a new
-        //   list with the images that have been updated.
-        for (cachedImage in imageFiles.first) {
+        val cacheImageFiles = imageFiles.first
+        val dataImageFiles = imageFiles.second
+        val cacheImageFilesCount = cacheImageFiles.size
+        val dataImageFilesCount = dataImageFiles.size
+        val processedImageFilesCount = cacheImageFilesCount + dataImageFilesCount
 
+        // Remove all cached files
+        Timber.v("Removing all cached files...")
+        for ((f, cachedFile) in imageFiles.first.withIndex()) {
+            val vm = ValueMax(f, processedImageFilesCount)
+            updateProgress(PROGRESS_STEP_IMAGE_REFERENCING, vm.percentage)
+            cachedFile.deleteIfExists() // TODO: Result should be checked
+        }
+
+        // Create the relations between the downloaded files, and its references in Firebase Storage.
+        val referencedImageFiles = processImageFiles(firestore, imageFiles.second) {
+            val vm = ValueMax(it + cacheImageFilesCount, processedImageFilesCount)
+            updateProgress(PROGRESS_STEP_IMAGE_REFERENCING, vm.percentage)
         }
 
         Timber.v("Finished updating data.")
@@ -171,6 +190,96 @@ private constructor(appContext: Context, workerParams: WorkerParameters) :
         val imageFiles = iterateFiles(cacheFilesCount, dataFiles)
 
         return cacheImageFiles to imageFiles
+    }
+
+    /**
+     * Processes the image files from [imageFiles], and elaborates a list with the info required to
+     * update only the files that need to be downloaded again from the server.
+     * @author Arnau Mora
+     * @since 20210926
+     * @param firestore The [FirebaseFirestore] instance to access the database data.
+     * @param imageFiles The list of files to check for updates.
+     * @return A list of pairs of a File and a String. The first File is the matching image file
+     * from the Filesystem. The String is the reference of the file in the Firebase Cloud storage.
+     * @throws FirebaseFirestoreException When there occurs a problem while fetching data from the
+     * server.
+     * @throws IndexOutOfBoundsException Thrown when the loading of a collection group from firebase
+     * returns an exception, or can't be loaded, that has not been caught by
+     * [FirebaseFirestoreException]. This should never be thrown.
+     * @throws RuntimeException When there's a type conversion issue.
+     */
+    @Throws(
+        FirebaseFirestoreException::class,
+        IndexOutOfBoundsException::class,
+        RuntimeException::class
+    )
+    private suspend fun processImageFiles(
+        firestore: FirebaseFirestore,
+        imageFiles: List<File>,
+        progress: suspend (count: Int) -> Unit
+    ): List<Pair<File, String>> {
+        val collections = hashMapOf<String, QuerySnapshot>()
+        val builtImageRelationList = arrayListOf<Pair<File, String>>()
+
+        // Now, for each image, compare its metadata with the one from the server, and create a new
+        //   list with the images that have been updated.
+        for ((i, imageFile) in imageFiles.withIndex()) {
+            progress(i)
+            val imageName = imageFile.name
+            val hyphenPos = imageName.indexOf('-')
+            val namespace = imageName.substring(0, hyphenPos)
+            // Add 22 since the id of the objects is always length 20, and for substring the last
+            // index is non-inclusive.
+            val id = imageName.substring(hyphenPos + 1, hyphenPos + 22)
+
+            // Add the final "s" for making the namespace plural
+            val collectionName = "${namespace}s"
+            // Make sure that collections contains all the data required. This is used for caching
+            // the data, and not overloading the server
+            if (!collections.containsKey(collectionName))
+                collections[collectionName] = try {
+                    firestore.collectionGroup(collectionName)
+                        .get()
+                        .await()
+                } catch (e: FirebaseFirestoreException) {
+                    Timber.e(
+                        e,
+                        "Could not get the collection group named \"$collectionName\" from " +
+                                "Firebase Firestore."
+                    )
+                    throw e
+                }
+            // Now get all the collections that match the desired namespace
+            val namespacedCollections = collections[collectionName] ?: run {
+                Timber.e("Could not fetch collections with name $collectionName from server.")
+                throw IndexOutOfBoundsException(
+                    "There was an unhandled exception while loading the collections with name " +
+                            "\"$collectionName\" that didn't allow it to be stored in memory to" +
+                            "fetch data from the server."
+                )
+            }
+            val documents = namespacedCollections.documents
+            documents.find { it.id == id }?.let { document ->
+                try {
+                    val imageReference = document.getString("image") ?: throw RuntimeException()
+                    builtImageRelationList.add(imageFile to imageReference)
+                } catch (e: RuntimeException) {
+                    Timber.e(
+                        e,
+                        "Tried to get the \"image\" field from element at " +
+                                "\"${document.reference.path}\" but it was not an String."
+                    )
+                    throw e
+                }
+            } ?: run {
+                Timber.e(
+                    "Could not find the document of the image (id=\"$id\") in the collection" +
+                            "\"$collectionName\"."
+                )
+            }
+        }
+
+        return builtImageRelationList
     }
 
     companion object {
@@ -224,6 +333,13 @@ private constructor(appContext: Context, workerParams: WorkerParameters) :
          * @since 20210926
          */
         const val PROGRESS_STEP_IMAGE_PROCESS = "ImageProcessing"
+
+        /**
+         * The name of the image referencing step.
+         * @author Arnau Mora
+         * @since 20210926
+         */
+        const val PROGRESS_STEP_IMAGE_REFERENCING = "ImageReferencing"
 
         /**
          * Gets the [LiveData] with a list of [WorkInfo] matching all the jobs that are updating the
