46b38a6ab3b01c7cee135287bea0b13b20390585
Arnau Mora <arnyminer.z@gmail.com>
2021-12-14 19:58:40 +0100

    :sparkles: Added `translatableString`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/TranslatableString.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/TranslatableString.kt
@@ -2,6 +2,19 @@ package com.arnyminerz.escalaralcoiaicomtat.core.data
 
 import android.content.Context
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+
+/**
+ * Creates a [TranslatableString] for using inside Composable functions.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param res The resource string id of the string
+ * @param placeholders If wanted, the placeholders for replacing parts of the string
+ */
+@Composable
+fun translatableString(@StringRes res: Int, vararg placeholders: String): TranslatableString =
+    TranslatableString(stringResource(res).format(placeholders))
 
 class TranslatableString(private val string: String) {
     constructor(string: String, vararg placeholders: String) : this(string.format(placeholders))
