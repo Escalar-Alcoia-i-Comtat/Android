089e0f47a0369899ab8ad2ee4757945ada3658e3
Arnau Mora <arnyminer.z@gmail.com>
2021-12-14 21:05:52 +0100

    :sparkles: Using Jetpack Compose
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/IntroActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/IntroActivity.kt
@@ -1,21 +1,22 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.arnyminerz.escalaralcoiaicomtat.BuildConfig
-import com.arnyminerz.escalaralcoiaicomtat.R
-import com.arnyminerz.escalaralcoiaicomtat.activity.model.LanguageAppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.arnyminerz.escalaralcoiaicomtat.core.BuildConfig
+import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREF_SHOWN_INTRO
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.launch
-import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityIntroBinding
-import com.arnyminerz.escalaralcoiaicomtat.fragment.intro.BetaIntroFragment
-import com.arnyminerz.escalaralcoiaicomtat.fragment.intro.MainIntroFragment
-import com.arnyminerz.escalaralcoiaicomtat.fragment.intro.WarningIntroFragment
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.IntroPageData
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.IntroWindow
+import com.google.accompanist.pager.ExperimentalPagerApi
 import timber.log.Timber
 
-class IntroActivity : LanguageAppCompatActivity() {
+@ExperimentalMaterial3Api
+class IntroActivity : ComponentActivity() {
     companion object {
         /**
          * Tells whether or not the Intro page should be shown.
@@ -25,61 +26,41 @@ class IntroActivity : LanguageAppCompatActivity() {
         fun shouldShow(): Boolean = !PREF_SHOWN_INTRO.get()
     }
 
-    private var adapterViewPager: IntroPagerAdapter? = null
-
-    private lateinit var binding: ActivityIntroBinding
-
+    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityIntroBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
-        adapterViewPager = IntroPagerAdapter(this)
-        binding.viewPager.adapter = adapterViewPager
-        binding.viewPager.isUserInputEnabled = false
+        Timber.v("Launching")
 
-        binding.introNextFAB.setOnClickListener {
-            next()
+        setContent {
+            val introPages = mutableListOf(
+                IntroPageData(
+                    stringResource(R.string.intro_main_title, "Escalar Alcoià i Comtat"),
+                    stringResource(R.string.intro_main_message)
+                ),
+                IntroPageData(
+                    stringResource(R.string.intro_warning_title),
+                    stringResource(R.string.intro_warning_message)
+                )
+            ).apply {
+                if (BuildConfig.DEBUG)
+                // If debug build, warn user
+                    add(
+                        IntroPageData(
+                            stringResource(R.string.intro_beta_title),
+                            stringResource(R.string.intro_beta_message)
+                        )
+                    )
+            }
+            val context = LocalContext.current
+
+            IntroWindow(
+                introPages
+            ) {
+                Timber.v("Finished showing intro pages. Loading LoadingActivity")
+                PREF_SHOWN_INTRO.put(true)
+                context.startActivity(Intent(this, MainActivity::class.java))
+            }
         }
-    }
-
-    fun fabStatus(enabled: Boolean) {
-        binding.introNextFAB.isEnabled = enabled
-    }
-
-    fun next() {
-        val position = binding.viewPager.currentItem
-
-        if (position + 1 >= adapterViewPager!!.fragments.size) {
-            Timber.v("Finished showing intro pages. Loading LoadingActivity")
-            PREF_SHOWN_INTRO.put(true)
-            launch(LoadingActivity()::class.java)
-        } else {
-            if (binding.viewPager.currentItem == adapterViewPager!!.fragments.size - 2)
-                binding.introNextFAB.setImageResource(R.drawable.round_check_24)
-            binding.viewPager.currentItem++
-            Timber.v("Showing intro page ${binding.viewPager.currentItem}")
-        }
-    }
-
-    class IntroPagerAdapter(fragmentActivity: FragmentActivity) :
-        FragmentStateAdapter(fragmentActivity) {
-        val fragments = arrayListOf<Fragment>()
-
-        private val mainIntroFragment = MainIntroFragment()
-        private val warningIntroFragment = WarningIntroFragment()
-        private val betaIntroFragment = BetaIntroFragment()
-
-        init {
-            fragments.add(mainIntroFragment)
-            fragments.add(warningIntroFragment)
-            if (BuildConfig.DEBUG)
-                fragments.add(betaIntroFragment)
-        }
-
-        override fun getItemCount(): Int = fragments.size
-
-        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 }
