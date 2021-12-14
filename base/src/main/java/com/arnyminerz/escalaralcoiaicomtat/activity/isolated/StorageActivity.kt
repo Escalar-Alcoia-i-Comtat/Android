9de142e0698c9be6fe434f4d8a1644524a0d7af2
Arnau Mora <Arnyminer.Z@gmail.com>
2021-12-15 00:21:35 +0100

    :sparkles: Storage Activity now uses Jetpack Compose (#495)
    * :sparkles: Added `StorageManagerWindow`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :fire: Removed `StorageActivity`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :sparkles: Using Jetpack Compose for `StorageActivity`
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :globe_with_meridians: Moved strings
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :art: Made buttons fill max width
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>
    
    * :art: Adjusted buttons width
    
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/StorageActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/isolated/StorageActivity.kt
@@ -1,115 +1,30 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity.isolated
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.appsearch.app.SearchSpec
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.work.await
-import com.arnyminerz.escalaralcoiaicomtat.BuildConfig
-import com.arnyminerz.escalaralcoiaicomtat.R
-import com.arnyminerz.escalaralcoiaicomtat.activity.MainActivity
-import com.arnyminerz.escalaralcoiaicomtat.activity.model.LanguageAppCompatActivity
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREF_INDEXED_SEARCH
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.app
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.sharedPreferences
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.deleteDir
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.launch
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.storage.filesDir
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.uiContext
-import com.arnyminerz.escalaralcoiaicomtat.databinding.ActivityStorageBinding
-import com.google.android.material.snackbar.Snackbar
-import java.io.IOException
+import androidx.compose.material3.Scaffold
+import com.arnyminerz.escalaralcoiaicomtat.activity.LoadingActivity
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.isolated_screen.StorageManagerWindow
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.theme.AppTheme
 
-@ExperimentalMaterial3Api
-class StorageActivity : LanguageAppCompatActivity() {
-    private lateinit var binding: ActivityStorageBinding
-
+class StorageActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityStorageBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
 
-        binding.clearCacheButton.setOnClickListener {
-            try {
-                deleteDir(cacheDir)
-            } catch (ex: IOException) {
-                ex.printStackTrace()
-            } finally {
-                Snackbar.make(binding.storageLayout, R.string.toast_clear_ok, Snackbar.LENGTH_SHORT).show()
-                refreshView()
-            }
-        }
-
-        binding.clearStorageButton.setOnClickListener {
-            try {
-                deleteDir(cacheDir.parentFile)
-            } catch (ex: IOException) {
-                ex.printStackTrace()
-            } finally {
-                Snackbar.make(binding.storageLayout, R.string.toast_clear_ok, Snackbar.LENGTH_SHORT).show()
-                refreshView()
-            }
-        }
-
-        binding.clearDownloadsButton.setOnClickListener {
-            try {
-                deleteDir(filesDir(this))
-            } catch (ex: IOException) {
-                ex.printStackTrace()
-            } finally {
-                Snackbar.make(binding.storageLayout, R.string.toast_clear_ok, Snackbar.LENGTH_SHORT)
-                    .show()
-                refreshView()
-            }
-        }
-
-        binding.clearSettingsButton.setOnClickListener {
-            sharedPreferences.edit()?.clear()?.apply()
-            Snackbar.make(binding.storageLayout, R.string.toast_clear_ok, Snackbar.LENGTH_SHORT)
-                .show()
-            refreshView()
-        }
-
-        binding.clearDataButton.setOnClickListener {
-            binding.clearDataButton.isEnabled = false
-            doAsync {
-                val searchSession = app.searchSession
-                val searchSpec = SearchSpec.Builder()
-                    .addFilterPackageNames(BuildConfig.APPLICATION_ID)
-                    .setResultCountPerPage(10000)
-                    .build()
-                searchSession.remove("", searchSpec).await()
-                PREF_INDEXED_SEARCH.put(false)
-                uiContext {
-                    binding.clearDataButton.isEnabled = true
-                    Snackbar.make(
-                        binding.storageLayout,
-                        R.string.toast_clear_ok,
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                    refreshView()
+        setContent {
+            AppTheme {
+                Scaffold {
+                    StorageManagerWindow({
+                        startActivity(Intent(this, LoadingActivity::class.java))
+                    }) {
+                        startActivity(Intent(this, FeedbackActivity::class.java))
+                    }
                 }
             }
-        }
-
-        binding.launchAppButton.setOnClickListener {
-            launch(MainActivity::class.java)
-        }
-
-        binding.feedbackButton.setOnClickListener {
-            launch(FeedbackActivity::class.java)
-        }
-
-        refreshView()
-    }
-
-    private fun refreshView() {
-        with(binding) {
-            clearCacheButton.isEnabled = cacheDir.exists()
-            clearStorageButton.isEnabled = cacheDir.parentFile?.exists() ?: false
-            clearDownloadsButton.isEnabled = filesDir(this@StorageActivity).exists()
-            clearSettingsButton.isEnabled = true
         }
     }
 }
---
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/isolated_screen/StorageManageWindow.kt
@@ -0,0 +1,236 @@
+package com.arnyminerz.escalaralcoiaicomtat.core.ui.isolated_screen
+
+import android.content.SharedPreferences
+import androidx.annotation.StringRes
+import androidx.appsearch.app.AppSearchSession
+import androidx.appsearch.app.SearchSpec
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.work.await
+import com.arnyminerz.escalaralcoiaicomtat.core.R
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.App
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.PREF_INDEXED_SEARCH
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.sharedPreferences
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.deleteDir
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.storage.filesDir
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.io.File
+import java.io.IOException
+
+/**
+ * A generic button template for clearing something.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param buttonText The text of the button.
+ * @param valueLoad A function that should load the current enabled value of the button.
+ * @param deleteAction What to call when the user requests to remove whatever the button removes.
+ */
+@Composable
+private fun ClearButton(
+    @StringRes buttonText: Int,
+    valueLoad: suspend () -> Boolean,
+    deleteAction: suspend () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var enabled by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val successToastString = stringResource(R.string.toast_clear_ok)
+    val failureToastString = stringResource(R.string.toast_error_internal)
+
+    doAsync {
+        val value = valueLoad()
+        scope.launch { enabled = value }
+    }
+
+    Button(
+        modifier = modifier,
+        enabled = enabled,
+        onClick = {
+            doAsync {
+                try {
+                    scope.launch { enabled = false }
+                    deleteAction()
+                    scope.launch { toast(context, successToastString) }
+                } catch (e: IOException) {
+                    Timber.e(e, "Could not delete directory.")
+                    scope.launch { toast(context, failureToastString) }
+                } finally {
+                    val value = valueLoad()
+                    scope.launch { enabled = value }
+                }
+            }
+        }
+    ) {
+        Text(stringResource(buttonText))
+    }
+}
+
+/**
+ * A button for controlling the clearing of a directory, for example, the cache.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param buttonText The text of the button that clears the [directory].
+ * @param directory The directory to control.
+ */
+@Composable
+private fun ClearDirectoryButton(
+    @StringRes buttonText: Int,
+    directory: File?,
+    modifier: Modifier = Modifier
+) {
+    ClearButton(
+        buttonText = buttonText,
+        valueLoad = { directory?.exists() ?: false },
+        deleteAction = { deleteDir(directory) },
+        modifier = modifier
+    )
+}
+
+/**
+ * A button that clears all the contents from a shared prefs.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param buttonText The text of the button.
+ * @param sharedPreferences The preferences to clear.
+ */
+@Composable
+private fun ClearSettingsButton(
+    @StringRes buttonText: Int,
+    sharedPreferences: SharedPreferences,
+    modifier: Modifier = Modifier
+) {
+    ClearButton(
+        buttonText,
+        valueLoad = { true },
+        deleteAction = { sharedPreferences.edit().clear().apply() },
+        modifier = modifier
+    )
+}
+
+/**
+ * A button that clears all the cdata stored in the search session.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param buttonText The text of the button.
+ * @param searchSession The search session to clear.
+ */
+@Composable
+private fun ClearSearchSessionButton(
+    @StringRes buttonText: Int,
+    searchSession: AppSearchSession,
+    modifier: Modifier = Modifier
+) {
+    ClearButton(
+        buttonText,
+        valueLoad = { true },
+        deleteAction = {
+            val searchSpec = SearchSpec.Builder()
+                .addFilterPackageNames("com.arnyminerz.escalaralcoiaicomtat")
+                .setResultCountPerPage(10000)
+                .build()
+            searchSession.remove("", searchSpec).await()
+            PREF_INDEXED_SEARCH.put(false)
+        },
+        modifier = modifier
+    )
+}
+
+/**
+ * The Window displayed in the storage activity which allows the user to clear different parts
+ * of the app's contents.
+ * @author Arnau Mora
+ * @since 20211214
+ * @param launchApp When the user requests to launch the app, this will get called.
+ * @param sendFeedback When the user selects the option to send feedback.
+ */
+@Composable
+fun StorageManagerWindow(launchApp: () -> Unit, sendFeedback: () -> Unit) {
+    val context = LocalContext.current
+    val cacheDir = context.cacheDir
+    val storageDir = cacheDir.parentFile
+
+    val app = context.applicationContext as App
+    val searchSession = app.searchSession
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(1f)
+        ) {
+            ClearDirectoryButton(
+                R.string.action_clear_cache,
+                cacheDir,
+                modifier = Modifier
+                    .fillMaxWidth(.5f)
+                    .padding(end = 4.dp)
+            )
+            ClearDirectoryButton(
+                R.string.action_clear_storage,
+                storageDir,
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .padding(start = 4.dp)
+            )
+        }
+        Row(modifier = Modifier.fillMaxWidth(1f)) {
+            ClearDirectoryButton(
+                R.string.action_clear_downloads,
+                filesDir(context),
+                modifier = Modifier
+                    .fillMaxWidth(.5f)
+                    .padding(end = 4.dp)
+            )
+            ClearSettingsButton(
+                R.string.action_clear_settings,
+                sharedPreferences,
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .padding(start = 4.dp)
+            )
+        }
+
+        ClearSearchSessionButton(
+            buttonText = R.string.action_clear_data, searchSession = searchSession,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+        )
+
+        Button(
+            onClick = launchApp,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+        ) {
+            Text(stringResource(R.string.action_launch_app))
+        }
+
+        Button(
+            onClick = sendFeedback,
+            modifier = Modifier
+                .fillMaxWidth(1f)
+        ) {
+            Text(stringResource(R.string.action_send_feedback))
+        }
+    }
+}
