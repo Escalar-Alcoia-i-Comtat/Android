60e22a2f15e8d33d12e72c97f7d1e2dfde6c3707
Arnau Mora <Arnyminer.Z@gmail.com>
2021-12-24 12:26:33 +0100

    :fire: Removed old Compose files (#512)

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/ViewModelExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel
-
-import android.app.Application
-import android.content.Context
-import androidx.lifecycle.AndroidViewModel
-
-val AndroidViewModel.context: Context
-    get() = getApplication<Application>().applicationContext
