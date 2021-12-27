64c2fc826044f431c019a2fd541243cec60db3c3
Arnau Mora <arnyminer.z@gmail.com>
2021-12-27 14:16:39 +0100

    :fire: Removed preview
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/LoadingActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/LoadingActivity.kt
@@ -14,14 +14,14 @@ import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arnyminerz.escalaralcoiaicomtat.R
@@ -211,16 +211,4 @@ fun LoadingWindow(
                 .fillMaxWidth()
         )
     }
-}
-
-@Preview(showSystemUi = true)
-@Composable
-fun LoadingWindowPreview() {
-    var shouldShowMessage by remember { mutableStateOf(false) }
-
-    /*Button(onClick = { shouldShowMessage = !shouldShowMessage }) {
-        Text(text = "Switch")
-    }*/
-
-    LoadingWindow("Loading...", shouldShowMessage, "This is a testing message")
 }
