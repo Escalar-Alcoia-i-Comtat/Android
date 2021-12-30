69e615c237d4f89e4f8d477335bad43a7573f986
Arnau Mora <arnyminer.z@gmail.com>
2021-12-30 13:49:10 +0100

    :art: Added map night mode
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/map/GoogleMap.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/map/GoogleMap.kt
@@ -1,9 +1,13 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.ui.map
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.maps.android.ktx.awaitMap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -19,6 +23,8 @@ import kotlinx.coroutines.launch
 @Composable
 fun GoogleMap(modifier: Modifier = Modifier, onLoad: ((googleMap: GoogleMap) -> Unit)? = null) {
     val mapViewState = rememberMapViewWithLifecycle()
+    val darkMode = isSystemInDarkTheme()
+    val context = LocalContext.current
 
     AndroidView(
         { mapViewState },
@@ -26,6 +32,8 @@ fun GoogleMap(modifier: Modifier = Modifier, onLoad: ((googleMap: GoogleMap) -> 
     ) { mapView ->
         CoroutineScope(Dispatchers.Main).launch {
             val map = mapView.awaitMap()
+            if (darkMode)
+                map.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.map_night))
             onLoad?.let { it(map) }
         }
     }
