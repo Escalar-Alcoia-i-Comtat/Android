fa9a42eb88d7efcb3554d2b65ca958b702ee2cfb
Arnau Mora <arnyminer.z@gmail.com>
2022-01-01 20:11:56 +0100

    :sparkles: Added `downloadId`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/downloads/DownloadedData.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/downloads/DownloadedData.kt
@@ -3,10 +3,6 @@ package com.arnyminerz.escalaralcoiaicomtat.core.data.climb.downloads
 import androidx.appsearch.annotation.Document
 import androidx.appsearch.app.AppSearchSchema.StringPropertyConfig.INDEXING_TYPE_EXACT_TERMS
 import com.arnyminerz.escalaralcoiaicomtat.core.annotations.ObjectId
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.App
 
 /**
  * Used for indexing the downloaded dataclasses.
@@ -15,27 +11,15 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.App
  */
 @Document
 data class DownloadedData(
+    @Document.Id var downloadId: String,
     @ObjectId
-    @Document.Id var objectId: String,
+    @Document.StringProperty(indexingType = INDEXING_TYPE_EXACT_TERMS) var objectId: String,
     @Document.CreationTimestampMillis var timestamp: Long,
     @Document.Namespace var namespace: String,
     @Document.StringProperty(indexingType = INDEXING_TYPE_EXACT_TERMS) var displayName: String,
     @Document.StringProperty(indexingType = INDEXING_TYPE_EXACT_TERMS) var path: String,
     @Document.LongProperty var childrenCount: Long,
+    @ObjectId
     @Document.StringProperty(indexingType = INDEXING_TYPE_EXACT_TERMS) var parentId: String,
     @Document.LongProperty var sizeBytes: Long
-) {
-    /**
-     * Converts the [DownloadedData] into a [DataClass].
-     * @author Arnau Mora
-     * @since 20211230
-     * @param app The [App] instance for fetching the values.
-     */
-    suspend fun export(app: App): DataClass<*, *> {
-        return when (namespace) {
-            Zone.NAMESPACE -> app.getZone(objectId)
-            Sector.NAMESPACE -> app.getSector(objectId)
-            else -> throw IllegalStateException("Only Zones and Sectors can be downloaded.")
-        } ?: throw ClassNotFoundException("Could not find a dataclass at $namespace:$objectId")
-    }
-}
+)
