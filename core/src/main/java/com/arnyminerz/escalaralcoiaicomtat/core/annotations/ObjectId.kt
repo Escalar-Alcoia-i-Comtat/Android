4c0abb016e33c1325ba3e99e22a312d35cfac0d5
Arnau Mora <arnyminer.z@gmail.com>
2022-01-01 13:23:40 +0100

    :sparkles: Added annotations
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/ObjectId.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/ObjectId.kt
@@ -1,3 +1,5 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.annotations
 
+@Target(AnnotationTarget.TYPE, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.SOURCE)
 annotation class ObjectId()
