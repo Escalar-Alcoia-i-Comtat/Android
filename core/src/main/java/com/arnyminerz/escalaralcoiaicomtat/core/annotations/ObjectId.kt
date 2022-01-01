9b7a54681571c8ca09bd961a211d0ac39ddabdce
Arnau Mora <arnyminer.z@gmail.com>
2022-01-01 14:29:24 +0100

    :sparkles: Added `PROPERTY` target
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/ObjectId.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/ObjectId.kt
@@ -1,5 +1,5 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.annotations
 
-@Target(AnnotationTarget.TYPE, AnnotationTarget.VALUE_PARAMETER)
+@Target(AnnotationTarget.TYPE, AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.PROPERTY)
 @Retention(AnnotationRetention.SOURCE)
 annotation class ObjectId()
