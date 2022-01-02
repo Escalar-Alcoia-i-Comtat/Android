bb7220df332f23b2a8220e14349f3429dd04fa0c
Arnau Mora <arnyminer.z@gmail.com>
2022-01-02 12:52:31 +0100

    :sparkles: Added root nav controller navigation
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/screen/main/Explore.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/screen/main/Explore.kt
@@ -10,20 +10,17 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
 import com.arnyminerz.escalaralcoiaicomtat.activity.MainActivity
-import com.arnyminerz.escalaralcoiaicomtat.activity.climb.AreaActivity
-import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_AREA
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.element.climb.DataClassItem
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.launch
-import com.arnyminerz.escalaralcoiaicomtat.core.utils.putExtra
 import com.google.android.material.badge.ExperimentalBadgeUtils
 import timber.log.Timber
 
 @Composable
 @ExperimentalBadgeUtils
 @OptIn(ExperimentalCoilApi::class, ExperimentalMaterial3Api::class)
-fun MainActivity.ExploreScreen() {
+fun MainActivity.ExploreScreen(rootNavController: NavController) {
     // TODO: Map and search bar
     val loadedAreas = exploreViewModel.loadedAreas
 
@@ -44,9 +41,7 @@ fun MainActivity.ExploreScreen() {
         items(exploreViewModel.areas) { area ->
             Timber.d("Displaying $area...")
             DataClassItem(area) {
-                launch(AreaActivity::class.java) {
-                    putExtra(EXTRA_AREA, area.objectId)
-                }
+                rootNavController.navigate(area.documentPath)
             }
         }
     }
