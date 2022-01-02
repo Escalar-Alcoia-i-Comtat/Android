4eeb421ba9480fe44a9c32f6362b5e9e1d43bb74
Arnau Mora <arnyminer.z@gmail.com>
2022-01-02 12:27:19 +0100

    :sparkles: Added `loadChildren`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/viewmodel/main/ExploreViewModel.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/viewmodel/main/ExploreViewModel.kt
@@ -7,7 +7,11 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.arnyminerz.escalaralcoiaicomtat.core.annotations.Namespace
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClassImpl
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.app
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -24,6 +28,20 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
 
     val areas = mutableStateListOf<Area>()
 
+    /**
+     * Used in DataClassExplorer for loading items during navigation. This list is dynamic.
+     * @author Arnau Mora
+     * @since 20220102
+     */
+    val dataClasses = mutableStateListOf<DataClassImpl>()
+
+    /**
+     * Tells whether or not [dataClasses] are being loaded.
+     * @author Arnau Mora
+     * @since 20220102
+     */
+    val loadingDataClasses = mutableStateOf(false)
+
     val loadedAreas = mutableStateOf(false)
 
     fun loadAreas() {
@@ -35,6 +53,33 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
                 areas.addAll(areasList)
                 loadedAreas.value = true
             }
+    }
+
+    /**
+     * Loads the children of a set DataClass, and stores them into [dataClasses].
+     * @author Arnau Mora
+     * @since 20220102
+     * @param namespace The namespace of the DataClass.
+     * @param objectId The id of the DataClass to load.
+     * @throws IllegalArgumentException If [namespace] is not valid.
+     */
+    @Throws(IllegalArgumentException::class)
+    fun loadChildren(@Namespace namespace: String, objectId: String) {
+        dataClasses.clear()
+        loadingDataClasses.value = true
+        viewModelScope.launch {
+            val dataClass = when (namespace) {
+                Area.NAMESPACE -> app.getArea(objectId)
+                Zone.NAMESPACE -> app.getZone(objectId)
+                Sector.NAMESPACE -> app.getSector(objectId)
+                else -> throw IllegalArgumentException("The namespace introduced is not valid.")
+            } ?: run {
+                throw ClassNotFoundException("The dataclass $namespace::$objectId could not be found.")
+            }
+            val children = dataClass.getChildren(app.searchSession)
+            dataClasses.addAll(children)
+            loadingDataClasses.value = false
+        }
     }
 
     class Factory(
