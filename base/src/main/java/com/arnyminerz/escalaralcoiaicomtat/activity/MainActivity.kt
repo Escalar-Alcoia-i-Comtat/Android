e07ed11a44f0bf4ebc0aea948b5fed6c64522dd8
Arnau Mora <arnyminer.z@gmail.com>
2022-01-02 12:29:18 +0100

    :sparkles: Added explorer navigation
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/MainActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/MainActivity.kt
@@ -9,26 +9,29 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.arnyminerz.escalaralcoiaicomtat.BuildConfig
 import com.arnyminerz.escalaralcoiaicomtat.activity.model.LanguageComponentActivity
 import com.arnyminerz.escalaralcoiaicomtat.core.preferences.PreferencesModule
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.NavItems
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.Screen
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.theme.AppTheme
+import com.arnyminerz.escalaralcoiaicomtat.ui.screen.explore.DataClassExplorer
 import com.arnyminerz.escalaralcoiaicomtat.ui.screen.main.DeveloperScreen
 import com.arnyminerz.escalaralcoiaicomtat.ui.screen.main.DownloadsScreen
 import com.arnyminerz.escalaralcoiaicomtat.ui.screen.main.ExploreScreen
 import com.arnyminerz.escalaralcoiaicomtat.ui.screen.main.MapScreen
 import com.arnyminerz.escalaralcoiaicomtat.ui.screen.main.SettingsScreen
-import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.DeveloperViewModel
-import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.DownloadsViewModel
-import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.ExploreViewModel
 import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.MainMapViewModel
-import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.SettingsViewModel
-import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.settingsViewModel
+import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main.DeveloperViewModel
+import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main.DownloadsViewModel
+import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main.ExploreViewModel
+import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main.SettingsViewModel
+import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main.settingsViewModel
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.material.badge.ExperimentalBadgeUtils
 
@@ -67,14 +70,34 @@ class MainActivity : LanguageComponentActivity() {
                 val navController = rememberNavController()
 
                 NavHost(navController, startDestination = "/") {
-                    /*composable(
+                    composable(
+                        "/Areas/{areaId}",
+                        arguments = listOf(
+                            navArgument("areaId") { type = NavType.StringType },
+                        )
+                    ) { entry ->
+                        DataClassExplorer(entry.arguments ?: Bundle())
+                    }
+                    composable(
+                        "/Areas/{areaId}/Zones/{zoneId}",
+                        arguments = listOf(
+                            navArgument("areaId") { type = NavType.StringType },
+                            navArgument("zoneId") { type = NavType.StringType },
+                        )
+                    ) { entry ->
+                        DataClassExplorer(entry.arguments ?: Bundle())
+                    }
+                    composable(
                         "/Areas/{areaId}/Zones/{zoneId}/Sectors/{sectorId}",
                         arguments = listOf(
                             navArgument("areaId") { type = NavType.StringType },
                             navArgument("zoneId") { type = NavType.StringType },
-                            navArgument("sectorId") { type = NavType.StringType }
+                            navArgument("sectorId") { type = NavType.StringType },
                         )
-                    ) {*/
+                    ) { entry ->
+                        DataClassExplorer(entry.arguments ?: Bundle())
+                    }
+
                     composable("/") {
                         Home()
                     }
