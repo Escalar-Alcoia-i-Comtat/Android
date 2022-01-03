a01b8262bada598ec8cd58bfaca29364a289b071
Arnau Mora <arnyminer.z@gmail.com>
2022-01-03 12:46:27 +0100

    :sparkles: Added `ExperimentalFoundationApi`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/MainActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.arnyminerz.escalaralcoiaicomtat.activity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.NavigationBar
@@ -71,7 +72,7 @@ class MainActivity : NetworkAwareComponentActivity() {
     internal val hasInternet = MutableLiveData<Boolean>()
 
     @ExperimentalBadgeUtils
-    @OptIn(ExperimentalMaterial3Api::class)
+    @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
