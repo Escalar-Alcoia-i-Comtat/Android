dd78610c619fa10f962b0db62beeae98d46b9bff
Arnau Mora <arnyminer.z@gmail.com>
2022-01-03 12:48:01 +0100

    :sparkles: Removed `DataClass` argument
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/worker/download/DownloadData.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/worker/download/DownloadData.kt
@@ -1,13 +1,13 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.worker.download
 
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_OVERWRITE_DEFAULT
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_QUALITY_DEFAULT
 
 class DownloadData
 /**
  * Initializes the class with specific parameters
- * @param dataClass The [DataClass] to download.
+ * @param path The path where the DataClass is at the database.
+ * @param displayName The display name of the DataClass.
  * @param overwrite If the download should be overwritten if already downloaded. Note that if this
  * is false, if the download already exists the task will fail.
  * @param quality The compression quality of the image
@@ -15,7 +15,8 @@ class DownloadData
  * @see DOWNLOAD_QUALITY_DEFAULT
  */
 constructor(
-    val dataClass: DataClass<*, *>,
+    val path: String,
+    val displayName: String,
     val overwrite: Boolean = DOWNLOAD_OVERWRITE_DEFAULT,
     val quality: Int = DOWNLOAD_QUALITY_DEFAULT
 )
