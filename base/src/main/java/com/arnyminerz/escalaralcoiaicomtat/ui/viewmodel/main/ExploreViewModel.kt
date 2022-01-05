67f67ff3c1806d3da21f18aa3ec96b6c62b04600
Arnau Mora <arnyminer.z@gmail.com>
2022-01-05 13:22:24 +0100

    :sparkles: Changed areas loading method
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/viewmodel/main/ExploreViewModel.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/ui/viewmodel/main/ExploreViewModel.kt
@@ -1,8 +1,6 @@
 package com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.main
 
 import android.app.Application
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -28,14 +26,12 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
         Timber.d("$this::onCleared")
     }
 
-    val areas = mutableStateListOf<Area>()
-
     /**
      * Used in DataClassExplorer for loading items during navigation. This list is dynamic.
      * @author Arnau Mora
      * @since 20220102
      */
-    val dataClasses = mutableStateListOf<DataClassImpl>()
+    val dataClasses = MutableLiveData<List<DataClassImpl>>()
 
     /**
      * The DataClass where [dataClasses] are stored.
@@ -45,23 +41,25 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
     val parentDataClass = MutableLiveData<DataClass<*, *>?>()
 
     /**
-     * Tells whether or not [dataClasses] are being loaded.
+     * Serves as a cache of the loaded areas so the map screen can access them.
      * @author Arnau Mora
-     * @since 20220102
+     * @since 20220105
      */
-    val loadingDataClasses = MutableLiveData<Boolean>()
+    var lastAreas = listOf<Area>()
+        private set
 
-    val loadedAreas = mutableStateOf(false)
-
-    fun loadAreas() {
-        if (!loadedAreas.value)
-            viewModelScope.launch {
-                val areasList = app.getAreas()
-                    .sortedBy { it.displayName }
-                areas.clear()
-                areas.addAll(areasList)
-                loadedAreas.value = true
-            }
+    /**
+     * Loads all the available areas, and posts them using a [MutableLiveData].
+     * @author Arnau Mora
+     * @since 20220105
+     */
+    fun loadAreas(): MutableLiveData<List<Area>> = MutableLiveData<List<Area>>().apply {
+        viewModelScope.launch {
+            val areasList = app.getAreas()
+                .sortedBy { it.displayName }
+            postValue(areasList)
+            lastAreas = areasList
+        }
     }
 
     /**
@@ -74,8 +72,7 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
      */
     @Throws(IllegalArgumentException::class)
     fun loadChildren(@Namespace namespace: String, objectId: String) {
-        dataClasses.clear()
-        loadingDataClasses.value = true
+        val newDataClasses = mutableListOf<DataClassImpl>()
         viewModelScope.launch {
             val dataClass = when (namespace) {
                 Area.NAMESPACE -> app.getArea(objectId)
@@ -87,8 +84,8 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
             }
             parentDataClass.postValue(dataClass)
             val children = dataClass.getChildren(app.searchSession)
-            dataClasses.addAll(children)
-            loadingDataClasses.value = false
+            newDataClasses.addAll(children)
+            dataClasses.postValue(newDataClasses)
         }
     }
 
@@ -98,9 +95,8 @@ class ExploreViewModel(application: Application) : AndroidViewModel(application)
      * @since 20220103
      */
     fun notifyNavigation() {
-        loadingDataClasses.value = true
         parentDataClass.value = null
-        dataClasses.clear()
+        dataClasses.postValue(listOf())
     }
 
     class Factory(
