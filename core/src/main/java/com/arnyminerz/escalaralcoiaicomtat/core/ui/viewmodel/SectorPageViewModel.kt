05f78ae7ac9854fd06983b133b021a1972b71353
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 19:55:57 +0100

    :sparkles: Updated model
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/SectorPageViewModel.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/SectorPageViewModel.kt
@@ -1,27 +1,36 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel
 
-import androidx.compose.runtime.MutableState
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.Path
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
-import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
 import me.bytebeats.views.charts.bar.BarChartData
 
 interface SectorPageViewModel {
     /**
-     * Gets the [BarChartData] of a DataClass.
+     * A Mutable state delegation of [BarChartData] with the data to display.
      * @author Arnau Mora
      * @since 20220106
-     * @return A [MutableState] that contains the [BarChartData] and gets updated with the correct
-     * data once loaded. May be null when still not loaded.
      */
-    fun getBarChartData(objectId: String): MutableState<BarChartData?>
+    var barChartData: BarChartData
 
     /**
-     * Loads a [Sector] contained in [Zone].
+     * A Mutable State delegation of a list of paths with the paths to display.
      * @author Arnau Mora
      * @since 20220106
-     * @param zone The [Zone] to load the [Sector] from.
-     * @param index The position of the [Sector] inside of the [Zone].
-     * @return A [MutableState] that contains a [Sector]. Is null while loading.
      */
-    fun loadSector(zone: Zone, index: Int): MutableState<Sector?>
+    var paths: List<Path>
+
+    /**
+     * Loads the [BarChartData] of [sector] into [loadBarChartData].
+     * @author Arnau Mora
+     * @since 20220106
+     */
+    fun loadBarChartData(sector: Sector)
+
+    /**
+     * Loads the list of [Path] contained in [sector].
+     * @author Arnau Mora
+     * @since 20220106
+     * @param sector The [Sector] to load the [Path] from.
+     */
+    fun loadPaths(sector: Sector)
 }
