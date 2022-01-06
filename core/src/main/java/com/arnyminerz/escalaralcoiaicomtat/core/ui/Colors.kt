66b1e1500310a1a55006aafbb4e74d3ab6c7bd59
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 19:54:18 +0100

    :fire: Removed `ItemTextBackground` and `ItemTextColor`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/Colors.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/Colors.kt
@@ -2,7 +2,4 @@ package com.arnyminerz.escalaralcoiaicomtat.core.ui
 
 import androidx.compose.ui.graphics.Color
 
-val ItemTextBackground = Color(0x4B000000)
-val ItemTextColor = Color(0xffeeeeee)
-
 val SearchItemTypeColor = Color(0xff303F9F) // Indigo 700
