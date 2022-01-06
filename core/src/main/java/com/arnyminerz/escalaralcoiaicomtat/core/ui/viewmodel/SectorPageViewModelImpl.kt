e98112eed941744b06f304d57a28dc7b244d696f
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 15:46:58 +0100

    :sparkles: Added demo implementation for `SectorPageViewModel`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/SectorPageViewModelImpl.kt
@@ -0,0 +1,47 @@
+package com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel
+
+import android.app.Application
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.arnyminerz.escalaralcoiaicomtat.core.R
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.context
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.color
+import me.bytebeats.views.charts.bar.BarChartData
+
+class SectorPageViewModelImpl(application: Application) : AndroidViewModel(application),
+    SectorPageViewModel {
+
+    override fun getBarChartData(objectId: String): MutableState<BarChartData> {
+        return mutableStateOf(
+            BarChartData(
+                bars = listOf(
+                    BarChartData.Bar(
+                        label = "Demo 1",
+                        value = 5f,
+                        color = ContextCompat.getColor(context, R.color.grade_green).color()
+                    ),
+                    BarChartData.Bar(
+                        label = "Demo 2",
+                        value = 7f,
+                        color = ContextCompat.getColor(context, R.color.grade_blue).color()
+                    )
+                )
+            )
+        )
+    }
+
+    class Factory(
+        private val application: Application,
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            @Suppress("UNCHECKED_CAST")
+            if (modelClass.isAssignableFrom(SectorPageViewModelImpl::class.java))
+                return SectorPageViewModelImpl(application) as T
+            error("Unknown view model class: $modelClass")
+        }
+    }
+}
