f831f196ee5bbbeb49010f4cfe6c973fe926a0e3
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 16:20:14 +0100

    :sparkles: Implemented sectors loading
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/SectorPageViewModelImpl.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/viewmodel/SectorPageViewModelImpl.kt
@@ -7,31 +7,57 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.arnyminerz.escalaralcoiaicomtat.core.R
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.app
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.context
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.color
+import kotlinx.coroutines.launch
 import me.bytebeats.views.charts.bar.BarChartData
+import timber.log.Timber
 
 class SectorPageViewModelImpl(application: Application) : AndroidViewModel(application),
     SectorPageViewModel {
 
-    override fun getBarChartData(objectId: String): MutableState<BarChartData> {
-        return mutableStateOf(
-            BarChartData(
-                bars = listOf(
-                    BarChartData.Bar(
-                        label = "Demo 1",
-                        value = 5f,
-                        color = ContextCompat.getColor(context, R.color.grade_green).color()
-                    ),
-                    BarChartData.Bar(
-                        label = "Demo 2",
-                        value = 7f,
-                        color = ContextCompat.getColor(context, R.color.grade_blue).color()
-                    )
+    override fun getBarChartData(objectId: String): MutableState<BarChartData?> {
+        val data = mutableStateOf<BarChartData?>(null)
+
+        data.value = BarChartData(
+            bars = listOf(
+                BarChartData.Bar(
+                    label = "Demo 1",
+                    value = 5f,
+                    color = ContextCompat.getColor(context, R.color.grade_green).color()
+                ),
+                BarChartData.Bar(
+                    label = "Demo 2",
+                    value = 7f,
+                    color = ContextCompat.getColor(context, R.color.grade_blue).color()
                 )
             )
         )
+
+        return data
+    }
+
+    override fun loadSectors(zoneId: String): MutableState<List<Sector>> {
+        val state = mutableStateOf<List<Sector>>(emptyList())
+
+        viewModelScope.launch {
+            // First get the parent zone
+            val zone = app.getZone(zoneId) ?: run {
+                // If the zone was not found
+                Timber.e("Zone with id $zoneId could not be found!")
+                return@launch
+            }
+            // Then get the children sectors
+            val sectors = zone.getChildren(app.searchSession)
+            // And pass them to the state
+            state.value = sectors
+        }
+
+        return state
     }
 
     class Factory(
