8514c7c989513ce1144d5aeafa5b20c736448065
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 16:36:08 +0100

    :sparkles: Added `Bundle.map`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/BundleUtils.kt
@@ -0,0 +1,13 @@
+package com.arnyminerz.escalaralcoiaicomtat.core.utils
+
+import android.os.Bundle
+
+/**
+ * Converts the [Bundle]'s contents into a map.
+ * @author Arnau Mora
+ * @since 20220106
+ */
+fun Bundle.toMap(): Map<String, Any?> = mutableMapOf<String, Any?>().apply {
+    for (key in keySet())
+        put(key, get(key))
+}
