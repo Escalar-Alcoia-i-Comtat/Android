852ff6b52316d1bc9d882ad20e4ba5c73303be34
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 22:14:35 +0100

    :sparkles: Added `EndingType.drawable`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/EndingTypeAnnotation.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/EndingTypeAnnotation.kt
@@ -1,6 +1,8 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.annotations
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringDef
+import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.ENDING_TYPE_CHAIN_CARABINER
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.ENDING_TYPE_CHAIN_RING
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.ENDING_TYPE_LANYARD
@@ -29,3 +31,18 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.ENDING_TYPE_WALKING
 )
 @Retention(AnnotationRetention.SOURCE)
 annotation class EndingType
+
+val @receiver:EndingType String.drawable: Int
+    @DrawableRes
+    get() = when (this) {
+        ENDING_TYPE_PLATE -> R.drawable.ic_reunio_xapes_24
+        ENDING_TYPE_PLATE_RING -> R.drawable.ic_reunio_xapesargolla
+        ENDING_TYPE_PLATE_LANYARD -> R.drawable.ic_lanyard
+        ENDING_TYPE_LANYARD -> R.drawable.ic_reunio_pont_de_roca
+        ENDING_TYPE_CHAIN_RING -> R.drawable.ic_reunio_cadenaargolla
+        ENDING_TYPE_CHAIN_CARABINER -> R.drawable.ic_reunio_cadenamosqueto
+        ENDING_TYPE_PITON -> R.drawable.ic_reunio_clau
+        ENDING_TYPE_WALKING -> R.drawable.ic_descens_caminant
+        ENDING_TYPE_RAPPEL -> R.drawable.ic_via_rappelable
+        else -> R.drawable.round_close_24
+    }
