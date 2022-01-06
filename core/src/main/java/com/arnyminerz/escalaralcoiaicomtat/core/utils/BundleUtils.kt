fe42c65553f4bc81c2fb1e5dca85322069dff50c
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 18:06:22 +0100

    :sparkles: Added `Bundle.sizeInBytes`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/BundleUtils.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/BundleUtils.kt
@@ -1,6 +1,7 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.utils
 
 import android.os.Bundle
+import android.os.Parcel
 
 /**
  * Converts the [Bundle]'s contents into a map.
@@ -10,4 +11,18 @@ import android.os.Bundle
 fun Bundle.toMap(): Map<String, Any?> = mutableMapOf<String, Any?>().apply {
     for (key in keySet())
         put(key, this@toMap.get(key))
+}
+
+/**
+ * Returns the size in bytes of the [Bundle].
+ * @author Arnau Mora
+ * @since 20220106
+ * @see <a href="https://stackoverflow.com/a/48447209">StackOverflow</a>
+ */
+fun Bundle.sizeInBytes(): Int {
+    val parcel = Parcel.obtain()
+    parcel.writeBundle(this)
+    val size = parcel.dataSize()
+    parcel.recycle()
+    return size
 }
