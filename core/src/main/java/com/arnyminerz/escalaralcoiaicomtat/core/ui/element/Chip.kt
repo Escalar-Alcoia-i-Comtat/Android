b3dfeb52506a11f7e5ae03e95803fc7412d86d82
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 22:35:50 +0100

    :sparkles: Added icon tint
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/element/Chip.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/element/Chip.kt
@@ -1,11 +1,13 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.ui.element
 
 import android.graphics.drawable.Drawable
+import androidx.annotation.ColorInt
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.graphics.drawable.DrawableCompat
 import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.google.android.material.chip.Chip
 
@@ -14,6 +16,7 @@ private fun rememberChipWithLifecycle(
     text: String,
     enabled: Boolean = true,
     icon: Drawable? = null,
+    @ColorInt iconTint: Int? = null,
     onClick: (() -> Unit)? = null
 ): Chip {
     val context = LocalContext.current
@@ -23,7 +26,12 @@ private fun rememberChipWithLifecycle(
             this.text = text
             this.isEnabled = enabled
             if (icon != null)
-                this.chipIcon = icon
+                this.chipIcon = if (iconTint != null)
+                    DrawableCompat.wrap(icon).apply {
+                        DrawableCompat.setTint(this, iconTint)
+                    }
+                else
+                    icon
             this.setOnClickListener { onClick?.let { it() } }
         }
     }
@@ -45,12 +53,18 @@ fun Chip(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     icon: Drawable? = null,
+    @ColorInt iconTint: Int? = null,
     onClick: (() -> Unit)? = null
 ) {
-    val chipState = rememberChipWithLifecycle(text, enabled, icon, onClick)
+    val chipState = rememberChipWithLifecycle(text, enabled, icon, iconTint, onClick)
     AndroidView(factory = { chipState }, modifier, update = {
         it.text = text
         it.isEnabled = enabled
-        it.chipIcon = icon
+        it.chipIcon = if (icon != null && iconTint != null)
+            DrawableCompat.wrap(icon).apply {
+                DrawableCompat.setTint(this, iconTint)
+            }
+        else
+            icon
     })
 }
