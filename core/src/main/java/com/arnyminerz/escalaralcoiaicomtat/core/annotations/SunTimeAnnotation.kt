88a9036ea03fc3178104a71b0613208733514856
Arnau Mora <arnyminer.z@gmail.com>
2022-01-06 15:47:11 +0100

    :sparkles: Added `textResource` and `icon`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/SunTimeAnnotation.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/annotations/SunTimeAnnotation.kt
@@ -1,6 +1,9 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.annotations
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.IntDef
+import androidx.annotation.StringRes
+import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.AFTERNOON
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.ALL_DAY
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.MORNING
@@ -9,3 +12,28 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.NO_SUN
 @IntDef(ALL_DAY, MORNING, AFTERNOON, NO_SUN)
 @Retention(AnnotationRetention.SOURCE)
 annotation class SunTime
+
+/**
+ * Gets the string resource that should be using to represent the sun time.
+ * @author Arnau Mora
+ * @since 20220106
+ */
+val @receiver:SunTime Int.textResource: Int
+    @StringRes
+    get() = when (this) {
+        ALL_DAY -> R.string.sector_sun_day
+        MORNING -> R.string.sector_sun_morning
+        AFTERNOON -> R.string.sector_sun_afternoon
+        NO_SUN -> R.string.sector_sun_no
+        else -> R.string.toast_error_internal
+    }
+
+val @receiver:SunTime Int.icon: Int
+    @DrawableRes
+    get() = when (this) {
+        ALL_DAY -> R.drawable.weather_sunny
+        MORNING -> R.drawable.weather_sunset_up
+        AFTERNOON -> R.drawable.weather_sunset_down
+        NO_SUN -> R.drawable.weather_partly_cloudy
+        else -> R.drawable.round_close_24
+    }
