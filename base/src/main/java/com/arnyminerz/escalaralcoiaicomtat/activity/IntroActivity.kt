2c2e707863e4994d16f81d5c156f41c314bba75d
Arnau Mora <arnyminer.z@gmail.com>
2022-01-30 14:07:01 +0100

    :sparkles: Added nearby zones switch actions
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/IntroActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/IntroActivity.kt
@@ -1,11 +1,19 @@
 package com.arnyminerz.escalaralcoiaicomtat.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -14,12 +22,19 @@ import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.arnyminerz.escalaralcoiaicomtat.core.preferences.PreferencesModule
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.IntroPageData
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.IntroWindow
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.action.IntroAction
+import com.arnyminerz.escalaralcoiaicomtat.core.ui.intro.action.IntroActionType
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.theme.AppTheme
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.launch
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.uiContext
 import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.IntroViewModel
 import com.arnyminerz.escalaralcoiaicomtat.ui.viewmodel.introViewModelFactory
 import com.google.accompanist.pager.ExperimentalPagerApi
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
 @ExperimentalMaterial3Api
@@ -30,6 +45,31 @@ class IntroActivity : ComponentActivity() {
      * @since 20211229
      */
     private val viewModel by viewModels<IntroViewModel>(factoryProducer = { PreferencesModule.introViewModelFactory })
+
+    var permissionCallback: (granted: Boolean) -> Unit = { }
+
+    /**
+     * The permission request for asking for location access.
+     * @author Arnau Mora
+     * @since 20220130
+     */
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) ->
+                // Fine location granted
+                permissionCallback(true)
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) ->
+                // Coarse location granted
+                permissionCallback(true)
+            else -> {
+                // The permission was not granted
+                toast(R.string.toast_error_permission)
+                permissionCallback(false)
+            }
+        }
+    }
 
     @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,19 +87,60 @@ class IntroActivity : ComponentActivity() {
 
         setContent {
             AppTheme {
+                var mayRequestLocationPermissions by remember { mutableStateOf(false) }
+                var nearbyZonesSwitchEnabled by remember { mutableStateOf(true) }
+
                 val introPages = mutableListOf(
-                    IntroPageData<Any>(
+                    IntroPageData(
                         stringResource(R.string.intro_main_title, "Escalar Alcoià i Comtat"),
                         stringResource(R.string.intro_main_message)
                     ),
-                    IntroPageData<Any>(
+                    IntroPageData(
                         stringResource(R.string.intro_warning_title),
                         stringResource(R.string.intro_warning_message)
                     ),
-                    IntroPageData<Any>(
+                    IntroPageData(
                         stringResource(R.string.intro_nearbyzones_title),
-                        stringResource(R.string.intro_nearbyzones_message)
-                    )
+                        stringResource(R.string.intro_nearbyzones_message),
+                        IntroAction(
+                            stringResource(R.string.intro_nearbyzones_enable),
+                            runBlocking { PreferencesModule.getNearbyZonesEnabled().first() },
+                            IntroActionType.SWITCH,
+                            { checked ->
+                                nearbyZonesSwitchEnabled = false
+                                doAsync {
+                                    PreferencesModule.setNearbyZonesEnabled.invoke(checked)
+
+                                    permissionCallback = { permissionGranted ->
+                                        if (!permissionGranted) {
+                                            setState(false)
+                                            doAsync {
+                                                PreferencesModule.setNearbyZonesEnabled(false)
+                                            }
+                                        }
+                                        mayRequestLocationPermissions = false
+                                    }
+
+                                    mayRequestLocationPermissions = if (checked)
+                                        ContextCompat.checkSelfPermission(
+                                            this@IntroActivity,
+                                            Manifest.permission.ACCESS_COARSE_LOCATION
+                                        ) == PackageManager.PERMISSION_DENIED
+                                                && ContextCompat.checkSelfPermission(
+                                            this@IntroActivity,
+                                            Manifest.permission.ACCESS_FINE_LOCATION
+                                        ) == PackageManager.PERMISSION_DENIED
+                                    else false
+
+                                    uiContext {
+                                        nearbyZonesSwitchEnabled = true
+                                        toast(R.string.toast_nearby_zones_enabled)
+                                    }
+                                }
+                            },
+                            nearbyZonesSwitchEnabled
+                        )
+                    ),
                 ).apply {
                     if (BuildConfig.DEBUG)
                     // If debug build, warn user
@@ -72,7 +153,14 @@ class IntroActivity : ComponentActivity() {
                 }
 
                 IntroWindow(
-                    introPages
+                    introPages,
+                    fabPermissions = if (mayRequestLocationPermissions)
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        )
+                    else emptyArray(),
+                    requestPermissionLauncher = locationPermissionRequest,
                 ) {
                     Timber.v("Finished showing intro pages. Loading LoadingActivity")
                     viewModel.markIntroAsShown()
