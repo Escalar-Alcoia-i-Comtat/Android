2840f2db8acf34ef86705ccc56c7ff411ed09612
Arnau Mora <arnyminer.z@gmail.com>
2022-02-27 00:14:26 +0100

    :sparkles: Added `update`

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/updater/Updater.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/data/climb/updater/Updater.kt
@@ -4,13 +4,25 @@ import android.content.Context
 import androidx.annotation.IntDef
 import androidx.annotation.WorkerThread
 import androidx.appsearch.app.AppSearchSession
+import androidx.appsearch.app.PutDocumentsRequest
 import androidx.appsearch.app.SearchSpec
+import androidx.appsearch.exceptions.AppSearchException
 import androidx.work.await
 import com.android.volley.VolleyError
+import com.arnyminerz.escalaralcoiaicomtat.core.R
 import com.arnyminerz.escalaralcoiaicomtat.core.annotations.Namespace
 import com.arnyminerz.escalaralcoiaicomtat.core.annotations.ObjectId
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.area.Area
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.path.Path
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.sector.Sector
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.App
+import com.arnyminerz.escalaralcoiaicomtat.core.shared.REST_API_DATA_FETCH
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.REST_API_UPDATER_ENDPOINT
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.getJson
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.toast
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.uiContext
 import timber.log.Timber
 
 /**
@@ -107,16 +119,18 @@ suspend fun updateAvailable(
         val jsonData = context.getJson("$REST_API_UPDATER_ENDPOINT$youngestTime")
             .takeIf { it.has("result") }
             ?: return UPDATE_AVAILABLE_FAIL_CLIENT
+
         // Get the result
         val jsonResult = jsonData.getJSONObject("result")
+
         // Clear the singleton's possible stored updatable elements
-        UpdaterSingleton
-            .getInstance()
-            .updateAvailableObjects
-            .clear()
+        val updaterSingleton = UpdaterSingleton.getInstance()
+        updaterSingleton.updateAvailableObjects = mapOf()
+
         // Get the fields required
         val updateAvailable = jsonResult.getBoolean("updateAvailable")
         val fieldsAvailable = jsonResult.has("fields")
+
         // If updateAvailable is true, and also result has "fields", parse the updatable elements
         if (updateAvailable && fieldsAvailable) {
             // Get the fields list
@@ -131,9 +145,12 @@ suspend fun updateAvailable(
                         idList.add(array.getString(i))
                     idList.toList()
                 }
-                UpdaterSingleton
-                    .getInstance()
-                    .updateAvailableObjects[table] = ids
+                updaterSingleton.updateAvailableObjects = updaterSingleton
+                    .updateAvailableObjects
+                    .toMutableMap()
+                    .apply {
+                        put(table, ids)
+                    }
             }
             // If there were updatable fields, return UPDATE_AVAILABLE
             if (updatableFields.length() > 0)
@@ -174,5 +191,70 @@ class UpdaterSingleton {
      * @author Arnau Mora
      * @since 20220226
      */
-    val updateAvailableObjects: HashMap<@Namespace String, List<@ObjectId String>> = hashMapOf()
+    var updateAvailableObjects: Map<@Namespace String, List<@ObjectId String>> = mapOf()
+
+    suspend fun update(
+        context: Context,
+        @Namespace namespace: String,
+        @ObjectId objectId: String,
+        score: Int
+    ) {
+        Timber.i("Updating data of $objectId at $namespace...")
+        val at = namespace + "s"
+        try {
+            Timber.d("Making data request to server... $namespace/$objectId")
+            val jsonData = context.getJson("$REST_API_DATA_FETCH$at/$objectId")
+            Timber.d("Decoding server response... $namespace/$objectId")
+            val jsonResult = jsonData.getJSONObject("result")
+            val jsonElement = jsonResult.getJSONObject(objectId) ?: run {
+                Timber.e("Server returned invalid result. $namespace/$objectId")
+                return
+            }
+            Timber.d("Constructing data... $namespace/$objectId")
+            val data = when (namespace) {
+                Area.NAMESPACE -> Area(jsonElement, objectId)
+                Zone.NAMESPACE -> Zone(jsonElement, objectId)
+                Sector.NAMESPACE -> Sector(jsonElement, objectId)
+                Path.NAMESPACE -> Path(jsonElement, objectId)
+                else -> {
+                    Timber.e("Namespace ($namespace) is not valid.")
+                    return
+                }
+            }
+            val app = context.applicationContext as App
+            Timber.d("Getting AppSearch document... $namespace/$objectId")
+            val doc = if (data is DataClass<*, *, *>)
+                data.data(score)
+            else
+                (data as Path).data()
+            Timber.d("Putting document into search session... $namespace/$objectId")
+            Timber.v("Building PutDocumentsRequest... $namespace/$objectId")
+            val request = PutDocumentsRequest.Builder()
+                .addDocuments(doc)
+                .build()
+            Timber.v("Requesting documents put... $namespace/$objectId")
+            app.searchSession
+                .put(request)
+                .await()
+            Timber.v("Added to SearchSession, removing element from updateAvailableObjects...")
+            updateAvailableObjects = updateAvailableObjects
+                .toMutableMap()
+                .apply {
+                    val newNamespaceList = arrayListOf<String>()
+                    get(namespace)?.forEach { if (it != objectId) newNamespaceList.add(it) }
+                    Timber.v("Updated id list for $namespace: $newNamespaceList")
+                    put(namespace, newNamespaceList)
+                }
+        } catch (e: VolleyError) {
+            Timber.e(e, "Could not update element at $namespace with id $objectId.")
+            uiContext {
+                context.toast(R.string.toast_error_update)
+            }
+        } catch (e: AppSearchException) {
+            Timber.e(e, "Could not store element at $namespace with id $objectId.")
+            uiContext {
+                context.toast(R.string.toast_error_update)
+            }
+        }
+    }
 }
