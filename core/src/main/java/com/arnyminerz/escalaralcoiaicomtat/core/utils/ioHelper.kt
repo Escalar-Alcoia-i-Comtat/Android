905d0b7fe0296a424e2c31f88201aabd709a9d0c
Arnau Mora <arnyminer.z@gmail.com>
2022-02-28 23:09:10 +0100

    :fire: Removed unused functions
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/ioHelper.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/ioHelper.kt
@@ -1,10 +1,5 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.utils
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.database.Cursor
-import android.net.Uri
-import android.provider.MediaStore
 import java.io.File
 import java.text.CharacterIterator
 import java.text.StringCharacterIterator
@@ -36,25 +31,6 @@ fun File.deleteIfExists(): Boolean {
     return if (exists())
         deleteDirAndContents()
     else true
-}
-
-/**
- * Creates the directory named by this abstract pathname, including any necessary but nonexistent
- * parent directories. Note that if this operation fails it may have succeeded in creating some of
- * the necessary parent directories.
- * @author Arnau Mora
- * @since 20210416
- * @param shouldDeleteIfNotDir If the target [File] already exists, but it's not a directory, and
- * [shouldDeleteIfNotDir] is true, the already existing path will be deleted and recreated.
- */
-fun File.mkdirsIfNotExists(shouldDeleteIfNotDir: Boolean = true): Boolean {
-    return if (exists())
-        when {
-            isDirectory -> true
-            shouldDeleteIfNotDir -> delete() && mkdirs()
-            else -> false
-        }
-    else mkdirs()
 }
 
 fun dirSize(file: File): Long {
@@ -97,44 +73,3 @@ fun humanReadableByteCountBin(bytes: Long, locale: Locale = Locale.getDefault())
 }
 
 fun File.size(): Long = if (isDirectory) dirSize(this) else length()
-
-/**
- * Gets the file name from an Uri
- * @author Arnau Mora
- * @since 20210318
- * @param context The context to call from
- * @return The file name, or null if it could not be gotten
- */
-@SuppressLint("Range")
-fun Uri.fileName(context: Context): String? {
-    if (scheme.equals("file")) {
-        return lastPathSegment
-    } else {
-        var cursor: Cursor? = null
-        try {
-            cursor = context.contentResolver.query(
-                this, arrayOf(
-                    MediaStore.Images.ImageColumns.DISPLAY_NAME
-                ), null, null, null
-            )
-            if (cursor != null && cursor.moveToFirst()) {
-                return cursor.getString(cursor.getColumnIndex(MediaStore.Images.ImageColumns.DISPLAY_NAME))
-            }
-        } finally {
-            cursor?.close()
-        }
-    }
-    return null
-}
-
-/**
- * Gets the mime type of the uri
- * @author Arnau Mora
- * @since 20210318
- * @param context The context to call from
- * @return The mime type of the uri
- */
-fun Uri.mime(context: Context): String? {
-    val cr = context.contentResolver
-    return cr.getType(this)
-}
