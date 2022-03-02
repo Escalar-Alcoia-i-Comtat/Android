04ed4159c6d24c5e3a969e6a1f16c41db50e022b
Arnau Mora <arnyminer.z@gmail.com>
2022-03-02 15:52:54 +0100

    :truck: Using `GeoPoint` instead of `LatLng`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/LatLngExtension.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/utils/LatLngExtension.kt
@@ -3,10 +3,9 @@ package com.arnyminerz.escalaralcoiaicomtat.core.utils
 import android.content.Intent
 import android.location.Location
 import android.net.Uri
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
+import org.osmdroid.util.GeoPoint
 
-fun LatLng.toUri(showMarker: Boolean = false, markerTitle: String? = null): Uri {
+fun GeoPoint.toUri(showMarker: Boolean = false, markerTitle: String? = null): Uri {
     return Uri.parse(
         if (showMarker)
             "geo:0,0?q=$latitude,$longitude${
@@ -18,38 +17,32 @@ fun LatLng.toUri(showMarker: Boolean = false, markerTitle: String? = null): Uri 
 }
 
 /**
- * Creates an [Intent] for launching Google Maps app showing the [LatLng] specified.
+ * Creates an [Intent] for launching Google Maps app showing the [GeoPoint] specified.
  * @author Arnau Mora
  * @since 20210830
  * @param showMarker If Google Maps should be showing a marker in the location.
  * @param markerTitle The title of the shown marker.
  * @return An [Intent] ready to be started that shows Google Maps with the configuration set.
  */
-fun LatLng.mapsIntent(showMarker: Boolean = true, markerTitle: String? = null): Intent =
+fun GeoPoint.mapsIntent(showMarker: Boolean = true, markerTitle: String? = null): Intent =
     Intent(Intent.ACTION_VIEW, toUri(showMarker, markerTitle))
         .setPackage("com.google.android.apps.maps")
 
-fun Location.toLatLng(): LatLng = LatLng(latitude, longitude)
+fun Location.toGeoPoint(): GeoPoint = GeoPoint(latitude, longitude)
 
 /**
- * Converts a [LatLng] object into a [Location].
+ * Converts a [GeoPoint] object into a [Location].
  * @author Arnau Mora
  * @since 20210603
  */
-fun LatLng.toLocation(): Location {
+fun GeoPoint.toLocation(): Location {
     val location = Location(this.toString())
     location.latitude = this.latitude
     location.longitude = this.longitude
     return location
 }
 
-fun LatLngBounds.Builder.includeAll(points: Collection<LatLng>): LatLngBounds.Builder {
-    for (point in points)
-        include(point)
-    return this
-}
-
-fun LatLng.distanceTo(other: LatLng): Float {
+fun GeoPoint.distanceTo(other: GeoPoint): Float {
     val a = this.toLocation()
     val b = other.toLocation()
     return a.distanceTo(b)
