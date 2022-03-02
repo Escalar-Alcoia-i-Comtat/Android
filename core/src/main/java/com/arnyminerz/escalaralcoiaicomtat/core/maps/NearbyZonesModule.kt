f4a7315c6244194913c940fbb7d0a2342abc9c90
Arnau Mora <arnyminer.z@gmail.com>
2022-03-02 12:10:51 +0100

    :sparkles: Using system location provider

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/maps/NearbyZonesModule.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/maps/NearbyZonesModule.kt
@@ -3,52 +3,35 @@ package com.arnyminerz.escalaralcoiaicomtat.core.maps
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
-import android.os.Looper
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.annotation.UiThread
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.LocationSource
 
 class NearbyZonesModule
 @UiThread constructor(
     context: Context,
     private val locationListener: (location: Location) -> Unit
-) : LocationSource, LocationCallback() {
+) : LocationSource, LocationListener {
     private lateinit var listener: LocationSource.OnLocationChangedListener
 
-    private val fusedLocationClient: FusedLocationProviderClient =
-        LocationServices.getFusedLocationProviderClient(context)
+    private val locationManager: LocationManager =
+        context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
     @SuppressLint("MissingPermission")
     override fun activate(listener: LocationSource.OnLocationChangedListener) {
         this.listener = listener
 
-        val locationRequest = LocationRequest.create()
-            .apply {
-                interval = 10000
-                fastestInterval = 5000
-                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            }
-
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            this,
-            Looper.getMainLooper()
-        )
+        // Get updates every 10 seconds or 10 meters
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10f, this)
     }
 
     override fun deactivate() {
-        fusedLocationClient.removeLocationUpdates(this)
+        locationManager.removeUpdates(this)
     }
 
-    override fun onLocationResult(result: LocationResult) {
-        super.onLocationResult(result)
-        result.locations.forEach { location ->
-            listener.onLocationChanged(location)
-            locationListener(location)
-        }
+    override fun onLocationChanged(location: Location) {
+        listener.onLocationChanged(location)
+        locationListener(location)
     }
 }
