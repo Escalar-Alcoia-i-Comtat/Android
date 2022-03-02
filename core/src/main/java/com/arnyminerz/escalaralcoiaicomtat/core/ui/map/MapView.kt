0c3bcc4d5ab0761970d347674883e1af45ac866e
Arnau Mora <arnyminer.z@gmail.com>
2022-03-02 17:45:06 +0100

    :sparkles: Using Bing maps satellite
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/map/MapView.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/ui/map/MapView.kt
@@ -3,21 +3,34 @@ package com.arnyminerz.escalaralcoiaicomtat.core.ui.map
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import org.osmdroid.tileprovider.tilesource.bing.BingMapTileSource
 import org.osmdroid.views.MapView
 
 /**
  * A composable Google Map.
  * @author Arnau Mora
  * @since 20211230
+ * @param bingMapsKey The key for using bing maps.
  * @param modifier Modifiers to apply to the map.
  * @param onLoad This will get called once the map has been loaded.
  */
 @Composable
-fun MapView(modifier: Modifier = Modifier, onLoad: ((map: MapView) -> Unit)? = null) {
+fun MapView(
+    bingMapsKey: String?,
+    modifier: Modifier = Modifier,
+    onLoad: ((map: MapView) -> Unit)? = null
+) {
     val mapViewState = rememberMapViewWithLifecycle()
 
     AndroidView(
         { mapViewState },
         modifier
-    ) { mapView -> onLoad?.invoke(mapView) }
+    ) { mapView ->
+        BingMapTileSource.setBingKey(bingMapsKey)
+        val bing = BingMapTileSource(null)
+        bing.style = BingMapTileSource.IMAGERYSET_AERIALWITHLABELS
+        mapView.setTileSource(bing)
+
+        onLoad?.invoke(mapView)
+    }
 }
