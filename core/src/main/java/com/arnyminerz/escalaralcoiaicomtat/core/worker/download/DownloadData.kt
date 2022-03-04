2df752ce508927e83f01da65fd4a10f648f96ba6
Arnau Mora <arnyminer.z@gmail.com>
2022-03-04 15:40:12 +0100

    :sparkles: Added `namespace` and `objectId`

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/worker/download/DownloadData.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/worker/download/DownloadData.kt
@@ -1,5 +1,7 @@
 package com.arnyminerz.escalaralcoiaicomtat.core.worker.download
 
+import com.arnyminerz.escalaralcoiaicomtat.core.annotations.Namespace
+import com.arnyminerz.escalaralcoiaicomtat.core.annotations.ObjectId
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_OVERWRITE_DEFAULT
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.DOWNLOAD_QUALITY_DEFAULT
 
@@ -7,6 +9,8 @@ class DownloadData
 /**
  * Initializes the class with specific parameters
  * @param displayName The display name of the DataClass.
+ * @param namespace The namespace of the DataClass to download.
+ * @param objectId The id of the DataClass to download.
  * @param overwrite If the download should be overwritten if already downloaded. Note that if this
  * is false, if the download already exists the task will fail.
  * @param quality The compression quality of the image
@@ -15,6 +19,8 @@ class DownloadData
  */
 constructor(
     val displayName: String,
+    @Namespace val namespace: String,
+    @ObjectId val objectId: String,
     val overwrite: Boolean = DOWNLOAD_OVERWRITE_DEFAULT,
     val quality: Int = DOWNLOAD_QUALITY_DEFAULT
 )
