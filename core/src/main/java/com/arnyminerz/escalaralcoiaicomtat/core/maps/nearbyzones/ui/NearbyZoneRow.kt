7e425ade0d74aa58f0a9931d4f3902501ae50e38
Arnau Mora <arnyminer.z@gmail.com>
2022-04-06 21:26:55 +0100

    :sparkles: Added launch button
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/maps/nearbyzones/ui/NearbyZoneRow.kt
+++ core/src/main/java/com/arnyminerz/escalaralcoiaicomtat/core/maps/nearbyzones/ui/NearbyZoneRow.kt
@@ -2,15 +2,27 @@ package com.arnyminerz.escalaralcoiaicomtat.core.maps.nearbyzones.ui
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import com.arnyminerz.escalaralcoiaicomtat.core.R
+import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
+import com.arnyminerz.escalaralcoiaicomtat.core.dataClassExploreActivity
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.async
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.launchAsync
 import org.osmdroid.views.overlay.Marker
 
 private class NearbyZoneRowProvider : PreviewParameterProvider<Marker> {
@@ -24,23 +36,39 @@ private class NearbyZoneRowProvider : PreviewParameterProvider<Marker> {
 
 @Preview
 @Composable
+@ExperimentalMaterialApi
 @ExperimentalMaterial3Api
 fun NearbyZonesRow(
     @PreviewParameter(NearbyZoneRowProvider::class) marker: Marker,
 ) {
-    Card(
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Text(
-            text = marker.title,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    start = 8.dp,
-                    end = 8.dp,
-                    top = 4.dp,
-                    bottom = 4.dp,
+    val context = LocalContext.current
+    ListItem(
+        text = {
+            Text(
+                text = marker.title,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 8.dp,
+                        end = 8.dp,
+                        top = 4.dp,
+                        bottom = 4.dp,
+                    )
+            )
+        },
+        trailing = {
+            IconButton(
+                onClick = async {
+                    DataClass
+                        .getIntent(context, dataClassExploreActivity, marker.title)
+                        ?.launchAsync(context)
+                },
+            ) {
+                Icon(
+                    Icons.Rounded.ChevronRight,
+                    stringResource(R.string.action_view),
                 )
-        )
-    }
+            }
+        },
+    )
 }
