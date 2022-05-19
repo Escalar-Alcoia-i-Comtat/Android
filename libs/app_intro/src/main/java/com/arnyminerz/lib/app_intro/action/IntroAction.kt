602b7dbec93d05797249f4b62fc0b346690309fb
Arnau Mora <arnyminer.z@gmail.com>
2022-05-19 18:47:04 +0100

    :sparkles: `currentValue` is now `MutableState`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- libs/app_intro/src/main/java/com/arnyminerz/lib/app_intro/action/IntroAction.kt
+++ libs/app_intro/src/main/java/com/arnyminerz/lib/app_intro/action/IntroAction.kt
@@ -1,5 +1,8 @@
 package com.arnyminerz.lib.app_intro.action
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 /**
  * Sets the data for an action for an intro page.
  * @author Arnau Mora
@@ -7,7 +10,7 @@ package com.arnyminerz.lib.app_intro.action
  */
 data class IntroAction<in R : Any?>(
     val text: String,
-    val currentValue: @UnsafeVariance R,
+    val currentValue: MutableState<@UnsafeVariance R>,
     val type: IntroActionType<@UnsafeVariance R>,
     val callback: IntroActionContext<@UnsafeVariance R>.(value: R) -> Unit,
     val enabled: Boolean = true,
@@ -18,6 +21,6 @@ data class IntroAction<in R : Any?>(
          * @author Arnau Mora
          * @since 20220130
          */
-        val None = IntroAction("", null, IntroActionType.NONE, {})
+        val None = IntroAction("", mutableStateOf(null), IntroActionType.NONE, {})
     }
 }
