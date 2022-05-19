f2929e06138a1c1e41e4716b595173141461b239
Arnau Mora <arnyminer.z@gmail.com>
2022-05-19 18:47:57 +0100

    :bug: Fixed permissions screen for intro
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- libs/app_intro/src/main/java/com/arnyminerz/lib/app_intro/IntroPageData.kt
+++ libs/app_intro/src/main/java/com/arnyminerz/lib/app_intro/IntroPageData.kt
@@ -12,5 +12,31 @@ import com.arnyminerz.lib.app_intro.action.IntroAction
 data class IntroPageData<R>(
     val title: String,
     val content: String,
-    val action: IntroAction<R> = IntroAction.None
-)
+    val action: IntroAction<R> = IntroAction.None,
+    val permissions: Array<String>? = null,
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as IntroPageData<*>
+
+        if (title != other.title) return false
+        if (content != other.content) return false
+        if (action != other.action) return false
+        if (permissions != null) {
+            if (other.permissions == null) return false
+            if (!permissions.contentEquals(other.permissions)) return false
+        } else if (other.permissions != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = title.hashCode()
+        result = 31 * result + content.hashCode()
+        result = 31 * result + action.hashCode()
+        result = 31 * result + (permissions?.contentHashCode() ?: 0)
+        return result
+    }
+}
