e459bb0eac8fb45a2393270ddebd95ee2e58267f
Arnau Mora <arnyminer.z@gmail.com>
2022-08-29 14:59:47 +0200

    :sparkles: Updated network state listener
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/climb/DataClassActivity.kt
+++ base/src/main/java/com/arnyminerz/escalaralcoiaicomtat/activity/climb/DataClassActivity.kt
@@ -15,11 +15,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.MutableLiveData
-import com.arnyminerz.escalaralcoiaicomtat.activity.model.NetworkAwareComponentActivity
+import com.arnyminerz.escalaralcoiaicomtat.activity.model.NetworkAwareActivity
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClass
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.dataclass.DataClassImpl
 import com.arnyminerz.escalaralcoiaicomtat.core.data.climb.zone.Zone
-import com.arnyminerz.escalaralcoiaicomtat.core.network.base.ConnectivityProvider
+import com.arnyminerz.escalaralcoiaicomtat.core.network.ConnectivityStateListener
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_CHILDREN_COUNT
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_DATACLASS
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_DATACLASS_ID
@@ -28,6 +28,7 @@ import com.arnyminerz.escalaralcoiaicomtat.core.shared.EXTRA_NAV_STACK
 import com.arnyminerz.escalaralcoiaicomtat.core.shared.REQUEST_CODE_ERROR_NO_DATACLASS
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.theme.AppTheme
 import com.arnyminerz.escalaralcoiaicomtat.core.ui.viewmodel.SectorPageViewModelImpl
+import com.arnyminerz.escalaralcoiaicomtat.core.utils.doAsync
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.getExtra
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.getExtraOrSavedInstanceState
 import com.arnyminerz.escalaralcoiaicomtat.core.utils.put
@@ -46,7 +47,7 @@ import timber.log.Timber
  * @author Arnau Mora
  * @since 20220105
  */
-class DataClassActivity : NetworkAwareComponentActivity() {
+class DataClassActivity : NetworkAwareActivity() {
     /**
      * The View Model for doing async tasks in DataClassExplorer.
      * @author Arnau Mora
@@ -179,10 +180,13 @@ class DataClassActivity : NetworkAwareComponentActivity() {
         super.onSaveInstanceState(outState)
     }
 
-    override fun onStateChange(state: ConnectivityProvider.NetworkState) {
+    override fun onStateChange(state: ConnectivityStateListener.NetworkState) {
         super.onStateChange(state)
-        Timber.i("Updated network state. Internet: ${state.hasInternet}")
-        hasInternet.postValue(state.hasInternet)
+        doAsync {
+            val internetAvailable = state.isInternetAvailable
+            Timber.i("Updated network state. Internet: $internetAvailable")
+            hasInternet.postValue(internetAvailable)
+        }
     }
 
     /**
