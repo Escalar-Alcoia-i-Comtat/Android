c661bafd1a813a1efceddc2175e29d9d941b883d
Arnau Mora <arnyminerz@proton.me>
2023-05-03 12:21:21 +0200

    Added test for deletions
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- core/src/androidTest/java/com/arnyminerz/escalaralcoiaicomtat/core/blocking/TestBlockingFetch.kt
+++ core/src/androidTest/java/com/arnyminerz/escalaralcoiaicomtat/core/blocking/TestBlockingFetch.kt
@@ -19,31 +19,40 @@ import org.junit.Test
 
 class TestBlockingFetch {
     companion object {
-        private val DefaultBlockagesList = JSONObject().apply {
-            put("1", JSONObject().apply {
-                // {"id":3,"blocked":true,"type":"old","endDate":null}
-                put("id", 3)
-                put("blocked", true)
-                put("type", "old")
-                put("endDate", null)
-            })
-            put("2", JSONObject().apply {
-                // {"id":5,"blocked":true,"type":"build","endDate":"2023-08-03T00:00:00.000Z"}
-                put("id", 5)
-                put("blocked", true)
-                put("type", "build")
-                put("endDate", "2023-08-03T00:00:00.000Z")
-            })
-            put("3", JSONObject().apply {
-                put("id", 5)
-                put("blocked", false)
-                put("type", "build")
-                put("endDate", "2023-08-03T00:00:00.000Z")
-            })
+        private val blocking1 = JSONObject().apply {
+            // {"id":3,"blocked":true,"type":"old","endDate":null}
+            put("id", 3)
+            put("blocked", true)
+            put("type", "old")
+            put("endDate", null)
+        }
+        private val blocking2 = JSONObject().apply {
+            // {"id":5,"blocked":true,"type":"build","endDate":"2023-08-03T00:00:00.000Z"}
+            put("id", 5)
+            put("blocked", true)
+            put("type", "build")
+            put("endDate", "2023-08-03T00:00:00.000Z")
+        }
+        private val blocking3 = JSONObject().apply {
+            put("id", 5)
+            put("blocked", false)
+            put("type", "build")
+            put("endDate", "2023-08-03T00:00:00.000Z")
         }
 
         private val DefaultBlockages = JSONObject().apply {
-            put("result", DefaultBlockagesList)
+            put("result", JSONObject().apply {
+                put("1", blocking1)
+                put("2", blocking2)
+                put("3", blocking3)
+            })
+        }
+
+        private val CutBlockages = JSONObject().apply {
+            put("result", JSONObject().apply {
+                put("1", blocking1)
+                put("3", blocking3)
+            })
         }
     }
 
@@ -93,25 +102,49 @@ class TestBlockingFetch {
         // Verify that the mock was called
         coVerify { BlockStatusWorker.fetchAllBlockagesFromServer(any()) }
 
-        val blockingData = runBlocking { dao.getAllOnce() }
+        runBlocking { dao.getAllOnce() }.let { blockingData ->
+            // Size must match DefaultBlockagesList
+            assertEquals(2, blockingData.size)
 
-        // Size must match DefaultBlockagesList
-        assertEquals(2, blockingData.size)
-
-        // Make sure the loaded data matches the defined one in JSON (the last one is not blocked)
-        blockingData[0].let { data ->
-            assertEquals(3, data.id)
-            assertEquals("1", data.pathId)
-            assertNull(data.endDate)
-            assertEquals("old", data.rawBlockingType)
-            assertEquals("old", data.blockingType.idName)
+            // Make sure the loaded data matches the defined one in JSON (the last one is not blocked)
+            blockingData[0].let { data ->
+                assertEquals(3, data.id)
+                assertEquals("1", data.pathId)
+                assertNull(data.endDate)
+                assertEquals("old", data.rawBlockingType)
+                assertEquals("old", data.blockingType.idName)
+            }
+            blockingData[1].let { data ->
+                assertEquals(5, data.id)
+                assertEquals("2", data.pathId)
+                assertEquals(1691013600000, data.endDate?.time)
+                assertEquals("build", data.rawBlockingType)
+                assertEquals("build", data.blockingType.idName)
+            }
         }
-        blockingData[1].let { data ->
-            assertEquals(5, data.id)
-            assertEquals("2", data.pathId)
-            assertEquals(1691013600000, data.endDate?.time)
-            assertEquals("build", data.rawBlockingType)
-            assertEquals("build", data.blockingType.idName)
+
+        // Test that existing entries are updated correctly
+        runBlocking { BlockStatusWorker.blockStatusFetchRoutine(applicationContext) }
+
+
+        // Now the "server" should return all elements except one, check deletion
+        coEvery { BlockStatusWorker.fetchAllBlockagesFromServer(any()) } returns CutBlockages
+
+        // Run the routine
+        runBlocking { BlockStatusWorker.blockStatusFetchRoutine(applicationContext) }
+
+        runBlocking { dao.getAllOnce() }.let { blockingData ->
+            // Size must match CutBlockages, one should have been deleted
+            assertEquals(1, blockingData.size)
+
+            // Make sure the loaded data matches the defined one in JSON (the last one is not blocked)
+            blockingData[0].let { data ->
+                assertEquals(3, data.id)
+                assertEquals("1", data.pathId)
+                assertNull(data.endDate)
+                assertEquals("old", data.rawBlockingType)
+                assertEquals("old", data.blockingType.idName)
+            }
         }
     }
 }
